ce7cdf887635c6c56ea10e1dcb5780ab539eb6d9
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-20 19:04:24 +0100

    wip

--- src/Noll/Kernel/LLVM/IRType.hs
+++ src/Noll/Kernel/LLVM/IRType.hs
@@ -1,1 +1,33 @@
-module Noll.Kernel.LLVM.IRType where
+{-# LANGUAGE StrictData #-}
+
+module Noll.Kernel.LLVM.IRType (IRType (..)) where
+
+import Noll.Language (Name)
+
+-- | LLVM IR language types
+data IRType
+  = -- | Single-bit integer type
+    TInt1
+  | -- | 8-bit integer type
+    TInt8
+  | -- | 32-bit integer type
+    TInt32
+  | -- | 64-bit integer type
+    TInt64
+  | -- | 32-bit floating-point value type
+    TFloat
+  | -- | 64-bit floating-point value type
+    TDouble
+  | -- | Void type
+    TVoid
+  | -- | Function type (return type coupled with a list of formal parameter types)
+    TFun IRType [IRType]
+  | -- | Pointer type
+    TPtr IRType
+  | -- | Structure type (a collection of data members)
+    TStruct [IRType]
+  | -- | Named type located at the top level
+    TName Name IRType
+  | -- | Array type (elements sequentially arranged in memory)
+    TArray Int IRType
+  deriving (Show, Eq, Ord, Read)
