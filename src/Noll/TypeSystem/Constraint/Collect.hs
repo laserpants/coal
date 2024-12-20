650eaf5a1192511f7080ba7d1183262963d511ba
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-20 17:46:38 +0100

    wip

--- src/Noll/TypeSystem/Constraint/Collect.hs
+++ src/Noll/TypeSystem/Constraint/Collect.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE StrictData #-}
+
 module Noll.TypeSystem.Constraint.Collect where
+
+import Noll.TypeSystem.Constraint (Monomorphic (..))
+
+data ConstraintsContext o k = ConstraintsContext
+  { contextMonoset :: Monomorphic o k
+  }
+  deriving (Show, Eq, Ord, Read)
