43c2085a25fcde9125fbde6e36c6561f8db49428
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-20 14:52:09 +0100

    wip

--- src/Noll/Language/Type/Kind.hs
+++ src/Noll/Language/Type/Kind.hs
@@ -1,1 +1,13 @@
-module Noll.Language.Type.Kind where
+{-# LANGUAGE StrictData #-}
+
+module Noll.Language.Type.Kind (Kind (..)) where
+
+data Kind o
+  = Type
+  | Row
+  | Arrow (Kind o) (Kind o)
+  | Trait
+  | Variable o
+  deriving (Show, Eq, Ord, Read)
+
+infixr 1 `Arrow`
