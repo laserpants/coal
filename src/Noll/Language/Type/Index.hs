4390bb8df8510642c3d94d37dfdefaf53aadef20
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-21 23:36:57 +0100

    wip

---
+++ src/Noll/Language/Type/Index.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE StrictData #-}
+
+module Noll.Language.Type.Index (TypeIndex (..)) where
+
+data TypeIndex k = TypeIndex k Int
+  deriving (Show, Eq, Ord, Read, Functor, Foldable)
