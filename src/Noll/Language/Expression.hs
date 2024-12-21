bd074b550484877a0c8b2e94ee10cae230532a86
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-22 00:15:00 +0100

    wip

--- src/Noll/Language/Expression.hs
+++ src/Noll/Language/Expression.hs
@@ -3,9 +3,23 @@
 
 module Noll.Language.Expression (Expression (..)) where
 
+import Noll.Label (Label (..))
+import Noll.Language.Expression.Binding (Binding (..))
+import Noll.Language.Pattern (Pattern (..))
+import Noll.Language.Primitive (Primitive (..))
 import Noll.Utils (Some)
 
 data Expression t
   = -- | Function application
     Application t (Expression t) (Some (Expression t))
+  | -- | Lambda function expression
+    Lambda (Some (Pattern t)) (Expression t)
+  | -- | Let binding
+    Let (Some (Binding Expression t)) (Expression t)
+  | -- | Variable
+    Variable (Label t)
+  | -- | Data constructor
+    Constructor (Label t)
+  | -- | Literal expression
+    Literal Primitive
   deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
