31563231e0144b7785f323ae31309c3ed668dd0b
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-21 14:52:44 +0100

    wip

--- src/Noll/TypeSystem/Constraint/Collection.hs
+++ src/Noll/TypeSystem/Constraint/Collection.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 
 module Noll.TypeSystem.Constraint.Collection where
 
-import Noll.TypeSystem.Constraint (MonomorphicSet (..))
+import Control.Monad.RWS (MonadReader, MonadWriter, MonadState, MonadRWS, RWS)
+import Noll.TypeSystem.Constraint (MonomorphicSet (..), TypeConstraint (..))
 
 data ConstraintsContext o k = ConstraintsContext
   { contextMonomorphicSet :: MonomorphicSet o k
@@ -13,3 +15,14 @@ data ConstraintsContext o k = ConstraintsContext
 {-# INLINE overContextMonomorphicSet #-}
 overContextMonomorphicSet :: (MonomorphicSet o k -> MonomorphicSet o k) -> ConstraintsContext o k -> ConstraintsContext o k
 overContextMonomorphicSet fn ConstraintsContext{..} = ConstraintsContext{contextMonomorphicSet = fn contextMonomorphicSet, ..}
+
+newtype Constraints o k t a = Constraints {constraintsMonad :: RWS (ConstraintsContext o k) [TypeConstraint o k t] () a}
+  deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadReader (ConstraintsContext o k)
+    , MonadWriter [TypeConstraint o k t]
+    , MonadState ()
+    , MonadRWS (ConstraintsContext o k) [TypeConstraint o k t] ()
+    )
