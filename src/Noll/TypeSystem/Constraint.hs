6fe72fe97d67d4024193acfd846f9cf115ddeb5d
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-21 22:43:02 +0100

    wip

--- src/Noll/TypeSystem/Constraint.hs
+++ src/Noll/TypeSystem/Constraint.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 
 module Noll.TypeSystem.Constraint (MonomorphicSet (..), TypeConstraint (..)) where
@@ -11,6 +12,10 @@ import Noll.Language.Type.Scheme (Scheme (..))
 -- | Monomorphic type variable set
 newtype MonomorphicSet m = MonomorphicSet {monomorphicSet :: Set m}
   deriving (Show, Eq, Ord, Read, Semigroup, Monoid)
+
+{-# INLINE overMonomorphicSet #-}
+overMonomorphicSet :: (Set m -> Set m) -> MonomorphicSet m -> MonomorphicSet m
+overMonomorphicSet fn MonomorphicSet{..} = MonomorphicSet{monomorphicSet = fn monomorphicSet}
 
 data TypeConstraint o k t
   = Equality t t
