019a3914d00c956052d32e97e6d56fc8c6bc06dd
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-21 02:35:29 +0100

    wip

--- src/Noll/Language/Expression.hs
+++ src/Noll/Language/Expression.hs
@@ -5,13 +5,7 @@ module Noll.Language.Expression where
 
 import Noll.Utils (Some)
 
--- TODO
-data Annotation a = Annotation a
-  deriving (Show, Eq, Ord, Read)
-
 data Expression t
-  = -- | Type-annotated expression
-    Annotated (Annotation ()) (Expression t)
-  | -- | Function application
+  = -- | Function application
     Application t (Expression t) (Some (Expression t))
   deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
