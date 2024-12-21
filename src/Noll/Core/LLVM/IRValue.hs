f300434afbe819d879236fce413ccca33292df9f
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-21 03:43:58 +0100

    wip

--- src/Noll/Core/LLVM/IRValue.hs
+++ src/Noll/Core/LLVM/IRValue.hs
@@ -1,1 +1,16 @@
 module Noll.Core.LLVM.IRValue where
+
+import Data.Int (Int32, Int64)
+import Noll.Core.LLVM.IRType (IRType)
+import Noll.Utils (Name)
+
+data IRValue
+  = Local IRType Name
+  | Global IRType Name
+  | I1 Bool
+  | I32 Int32
+  | I64 Int64
+  | Float Float
+  | Double Double
+  | Null
+  deriving (Show, Eq, Ord, Read)
