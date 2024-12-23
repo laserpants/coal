5c1bfee362c15811118b727970a2d25b4a14f8bb
Johannes Hildén <johannes@knodd.se>
2024-12-23 13:23:01 +0100

    wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import qualified Noll.Language.HasTypeIndexesSpec
 import qualified Noll.TypeSystem.Constraint.CollectSpec
+import qualified Noll.TypeSystem.SubstitutionSpec
 import Test.Hspec (hspec)
 
 main :: IO ()
@@ -7,3 +8,4 @@ main =
   hspec $ do
     Noll.TypeSystem.Constraint.CollectSpec.spec
     Noll.Language.HasTypeIndexesSpec.spec
+    Noll.TypeSystem.SubstitutionSpec.spec
