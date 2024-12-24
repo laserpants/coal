be09101cd7b05fb828c9015436cc8c870b3c249c
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-24 18:17:21 +0100

    wip

--- src/Noll/Utils.hs
+++ src/Noll/Utils.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE StrictData #-}
 
 module Noll.Utils (
+  module Data.Set,
+  module Data.Map.Strict,
   Name,
   Dictionary,
   IndexMap,
@@ -11,6 +13,7 @@ module Noll.Utils (
 
 import Data.List.NonEmpty (NonEmpty)
 import Data.Map.Strict (Map)
+import Data.Set (Set)
 import Data.Text (Text)
 
 type Name = Text
