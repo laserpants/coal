d349f20575d1d1c7fe3479291782b5f40688088d
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2024-12-24 21:12:48 +0100

    wip

---
+++ src/Noll/TypeSystem/KindConstraint/Solver.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StrictData #-}
+
+module Noll.TypeSystem.KindConstraint.Solver where
+
+import Control.Monad.State (MonadState)
+import Noll.TypeSystem.KindConstraint (KindConstraint (..))
+import Noll.TypeSystem.KindSubstitution (KindSubstitutable (..), KindSubstitution (..))
+import Noll.TypeSystem.KindUnification (KindUnifiable (..))
+
+solveKinds ::
+  ( KindSubstitutable (KindConstraint k)
+  , MonadState Int m
+  ) =>
+  [KindConstraint k] ->
+  m KindSubstitution
+solveKinds [] = pure mempty
+solveKinds (KindEquality k1 k2 : cs) = do
+  sub1 <- unifyKinds k1 k2
+  sub2 <- solveKinds (applyKindSub sub1 cs)
+  pure (sub2 <> sub1)
