c9813d88f7d192bf23af7445fa9c1a430263d96b
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-01 22:26:07 +0100

    wip

--- src/Noll/TypeSystem/KindConstraint.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE StrictData #-}
-
-module Noll.TypeSystem.KindConstraint (
-  KindConstraint (..),
-) where
-
-data KindConstraint c k = KindEquality c k k
-  deriving (Show, Eq, Ord, Read, Functor, Foldable)
--- src/Noll/TypeSystem/Unification/Error.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE StrictData #-}
-
-module Noll.TypeSystem.Unification.Error (UnificationError (..)) where
-
-data UnificationError
-  = CannotUnify
-  | InfiniteType
-  | CannotUnifyKinds
-  deriving (Show, Eq, Ord, Read)
