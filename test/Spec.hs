533ba39ec2ca107fca12d78904fc9ba0850a6a62
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-01 22:27:00 +0100

    wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,21 @@
 import qualified Noll.Language.IndexedSpec
-import qualified Noll.TypeSystem.ConstraintSolverSpec
-import qualified Noll.TypeSystem.KindConstraint.CollectSpec
-import qualified Noll.TypeSystem.KindSubstitutionSpec
-import qualified Noll.TypeSystem.TypeConstraint.CollectSpec
-import qualified Noll.TypeSystem.TypeSubstitutionSpec
-import qualified Noll.TypeSystem.TypeUnificationSpec
-import qualified Noll.TypeSystemSpec
+--import qualified Noll.TypeSystem.ConstraintSolverSpec
+--import qualified Noll.TypeSystem.KindConstraint.CollectSpec
+--import qualified Noll.TypeSystem.KindSubstitutionSpec
+--import qualified Noll.TypeSystem.TypeConstraint.CollectSpec
+--import qualified Noll.TypeSystem.TypeSubstitutionSpec
+--import qualified Noll.TypeSystem.TypeUnificationSpec
+--import qualified Noll.TypeSystemSpec
 import Test.Hspec (hspec, it)
 
 main :: IO ()
 main =
   hspec $ do
-    Noll.TypeSystem.TypeConstraint.CollectSpec.spec
     Noll.Language.IndexedSpec.spec
-    Noll.TypeSystem.TypeSubstitutionSpec.spec
-    Noll.TypeSystem.KindSubstitutionSpec.spec
-    Noll.TypeSystem.ConstraintSolverSpec.spec
-    Noll.TypeSystem.KindConstraint.CollectSpec.spec
-    Noll.TypeSystem.TypeUnificationSpec.spec
-    Noll.TypeSystemSpec.spec
+--    Noll.TypeSystem.TypeConstraint.CollectSpec.spec
+--    Noll.TypeSystem.TypeSubstitutionSpec.spec
+--    Noll.TypeSystem.KindSubstitutionSpec.spec
+--    Noll.TypeSystem.ConstraintSolverSpec.spec
+--    Noll.TypeSystem.KindConstraint.CollectSpec.spec
+--    Noll.TypeSystem.TypeUnificationSpec.spec
+--    Noll.TypeSystemSpec.spec
