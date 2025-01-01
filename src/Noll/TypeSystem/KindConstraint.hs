5ce83d1d2e181e3e7b7518b30ec30f220b816c58
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-01 14:03:52 +0100

    wip

--- src/Noll/TypeSystem/KindConstraint.hs
+++ src/Noll/TypeSystem/KindConstraint.hs
@@ -5,7 +5,5 @@ module Noll.TypeSystem.KindConstraint (
   KindConstraint (..),
 ) where
 
-import Noll.Language (OpaqueType (..))
-
 data KindConstraint c k = KindEquality c k k
   deriving (Show, Eq, Ord, Read, Functor, Foldable)
