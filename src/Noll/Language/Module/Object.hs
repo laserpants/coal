c86ec6d22bff78133070a575695f36d06a189b3a
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-13 19:25:07 +0100

    wip

--- src/Noll/Language/Module/Object.hs
+++ src/Noll/Language/Module/Object.hs
@@ -3,34 +3,44 @@
 
 module Noll.Language.Module.Object (Object (..)) where
 
+import Noll.Language.Constructor (Constructor (..))
 import Noll.Language.Expression (Expression (..))
 import Noll.Language.Module.Function (Function (..))
 import Noll.Language.Module.Global (Global (..))
+import Noll.Language.Trait (Trait (..), Uses (..))
+import Noll.Language.Type (Type (..), TypeIndex)
 import Noll.Utils (Name)
+
+type Annotation = Type TypeIndex
 
 newtype Path = Path {pathComponents :: [Name]}
   deriving (Show, Eq, Ord, Read)
 
+-- TODO: move
+data Instance e a t
+  = TFunction (Function e a t)
+  | TConstant (Global e a t)
+  deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
+
 data Object a k t
-  = -- | Codata type definition
+  = -- | Type definition
+    DType Name [Annotation k] [Constructor TypeIndex k (Type TypeIndex k)]
+  | -- | Codata type definition
     DCotype a Name -- TODO
   | -- | Function definition
     DFunction Name (Function Expression a t)
   | -- | TODO
     DGlobal Name (Global Expression a t)
-  | --  | -- | Type signature
-    --    DSignature Name (Uses (Annotation k))
-    --  | -- | Trait instance
-    --    DInstance Name (Annotation k) [(Name, Instance Expression t)]
-
-    -- | Import statement
+  | -- | Type signature
+    DSignature Name (Uses (Annotation k))
+  | -- | Import statement
     DImport Path [Name]
-  --  | -- | Trait
-  --    DTrait Name [Trait t] (Annotation k) [TypeId (Annotation k)]
-  --  | -- | Type definition
-  --    DType Name [Annotation k] [Constructor k (Type TypeId k)]
-  --  | -- | Type alias
-  --    DTypeAlias Name [Annotation k] (Type TypeId k)
+  | -- | Trait
+    DTrait Name [Trait t] (Annotation k) [TypeIndex (Annotation k)]
+  | --   | -- | Trait instance
+    DInstance Name (Annotation k) [(Name, Instance Expression a t)]
+  | -- | Type alias
+    DTypeAlias Name [Annotation k] (Type TypeIndex k)
   --  | -- | TODO
   --    DAnnotation (Uses (Annotation k)) (Object k t)
   deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
