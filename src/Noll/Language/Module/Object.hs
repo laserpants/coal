0b402bf01ae049983af4a75dedee70ffce29268d
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-14 00:55:04 +0100

    wip

--- src/Noll/Language/Module/Object.hs
+++ src/Noll/Language/Module/Object.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE StrictData #-}
 
-module Noll.Language.Module.Object (Object (..)) where
+module Noll.Language.Module.Object (Object (..), Path (..)) where
 
 import Noll.Language.Constructor (Constructor (..))
 import Noll.Language.Expression (Expression (..))
@@ -11,20 +11,12 @@ import Noll.Language.Trait (Trait (..), Uses (..))
 import Noll.Language.Type (Type (..), TypeIndex)
 import Noll.Utils (Name)
 
-type Annotation = Type TypeIndex
-
 newtype Path = Path {pathComponents :: [Name]}
   deriving (Show, Eq, Ord, Read)
 
--- TODO: move
-data Instance e a t
-  = TFunction (Function e a t)
-  | TConstant (Global e a t)
-  deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
-
 data Object a k t
   = -- | Type definition
-    DType Name [Annotation k] [Constructor TypeIndex k (Type TypeIndex k)]
+    DType Name [Type TypeIndex k] [Constructor TypeIndex k (Type TypeIndex k)]
   | -- | Codata type definition
     DCotype a Name -- TODO
   | -- | Function definition
@@ -32,15 +24,15 @@ data Object a k t
   | -- | TODO
     DGlobal Name (Global Expression a t)
   | -- | Type signature
-    DSignature Name (Uses (Annotation k))
+    DSignature Name (Uses (Type TypeIndex k))
   | -- | Import statement
     DImport Path [Name]
   | -- | Trait
-    DTrait Name [Trait t] (Annotation k) [TypeIndex (Annotation k)]
-  | --   | -- | Trait instance
-    DInstance Name (Annotation k) [(Name, Instance Expression a t)]
+    DTrait Name [Trait t] (Type TypeIndex k) [TypeIndex (Type TypeIndex k)]
+  | -- | Trait instance
+    DInstance Name (Type TypeIndex k) [Object a k t]
   | -- | Type alias
-    DTypeAlias Name [Annotation k] (Type TypeIndex k)
+    DTypeAlias Name [Type TypeIndex k] (Type TypeIndex k)
   --  | -- | TODO
-  --    DAnnotation (Uses (Annotation k)) (Object k t)
+  --    DType TypeIndex (Uses (Type TypeIndex k)) (Object k t)
   deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
