6ccc6fecbbd6adec1f84243f5d41aaeaa8cebfa0
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-17 04:45:49 +0100

    wip

--- test/Noll/Compiler/Transform/PatternBindingExpansionSpec.hs
+++ test/Noll/Compiler/Transform/PatternBindingExpansionSpec.hs
@@ -21,10 +21,11 @@ spec =
       runTranslatable "v" 0 (translate fixture5) == fixture6
     it "" $
       runTranslatable "v" 0 (translate fixture7) == fixture8
+    it "" $
+      runTranslatable "v" 0 (translate fixture9) == fixture10
 
 -- let
---  Some(p) =       Option(int32)
---    x             Option(int32)
+--  Some(p) = x     Option(int32)
 --  in
 --    p             int32
 --
@@ -42,8 +43,7 @@ fixture1 =
   )
 
 -- let
---  $v.0 =               Option(int32)
---    x                  Option(int32)
+--  $v.0 = x             Option(int32)
 --  in
 --    match(             int32
 --      $v.0             Option(int32)
@@ -121,8 +121,7 @@ fixture4 =
     )
 
 -- letrec
---  Some(p) =       Option(int32)
---    x             Option(int32)
+--  Some(p) = x     Option(int32)
 --  in
 --    p             int32
 --
@@ -136,8 +135,7 @@ fixture5 =
   )
 
 -- let
---  $v.0 =               Option(int32)
---    x                  Option(int32)
+--  $v.0 = x             Option(int32)
 --  in
 --    match(             int32
 --      $v.0             Option(int32)
@@ -510,3 +508,99 @@ fixture8 =
             "in_range"
         )
     )
+
+-- let
+--  Some(p) = x     Option(int32)
+--  Some(f) = y     Option(int32 -> int32)
+--  in
+--    f(p)          int32
+--
+fixture9 :: Expression () (Type TypeIndex ())
+fixture9 =
+  ( ELet
+      ()
+      ( BPattern
+          ()
+          (PConstructor () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "Some") [PVariable () (Label (TIntrinsic IInt32) "p")])
+          (EVariable () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "x"))
+          <| BPattern
+            ()
+            (PConstructor () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "Some") [PVariable () (Label (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) "f")])
+            (EVariable () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "y"))
+          :| []
+      )
+      ( EApplication
+          ()
+          (TIntrinsic IInt32)
+          (EVariable () (Label (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) "f"))
+          (EVariable () (Label (TIntrinsic IInt32) "p") :| [])
+      )
+  )
+
+-- let
+--  $v.0 = x                   Option(int32)
+--  $v.1 = y                   Option(int32 -> int32)
+--  in
+--    match(                   int32
+--      $v.0                   Option(int32)
+--    ) {
+--      | Some(p) =>           Option(int32)
+--          match(             int32
+--            $v.1             Option(int32 -> int32)
+--          ) {
+--            | Some(f) =>     Option(int32 -> int32)
+--                f(p)         int32
+--          }
+--    }
+--
+fixture10 :: Expression () (Type TypeIndex ())
+fixture10 =
+  ( ELet
+      ()
+      ( BPattern
+          ()
+          (PVariable () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "$v.0"))
+          (EVariable () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "x"))
+          <| BPattern
+            ()
+            (PVariable () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "$v.1"))
+            (EVariable () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "y"))
+          :| []
+      )
+      ( EMatch
+          ()
+          (TIntrinsic IInt32)
+          (EVariable () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "$v.0"))
+          ( EClause
+              ()
+              (PConstructor () (Label (TApplication () (TConstructor () "Option") (TIntrinsic IInt32 :| [])) "Some") [PVariable () (Label (TIntrinsic IInt32) "p")])
+              ( CPlain
+                  ()
+                  []
+                  ( EMatch
+                      ()
+                      (TIntrinsic IInt32)
+                      (EVariable () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "$v.1"))
+                      ( EClause
+                          ()
+                          (PConstructor () (Label (TApplication () (TConstructor () "Option") ((TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) :| [])) "Some") [PVariable () (Label (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) "f")])
+                          ( CPlain
+                              ()
+                              []
+                              ( EApplication
+                                  ()
+                                  (TIntrinsic IInt32)
+                                  (EVariable () (Label (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32) "f"))
+                                  (EVariable () (Label (TIntrinsic IInt32) "p") :| [])
+                              )
+                              :| []
+                          )
+                          :| []
+                      )
+                  )
+                  :| []
+              )
+              :| []
+          )
+      )
+  )
