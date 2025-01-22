c3fbbb8378dea053468b7bbace5d45599a26bcc3
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-01-22 23:42:34 +0100

    wip

--- src/Noll/Compiler/Transform/Pattern/Expansion.hs
+++ src/Noll/Compiler/Transform/Pattern/Expansion.hs
@@ -14,6 +14,7 @@ import Control.Monad.State (MonadState, State, evalState)
 import Control.Monad.Writer (MonadWriter, WriterT, runWriterT, tell)
 import Noll.Common.List1 (List1, NonEmpty ((:|)))
 import Noll.Common.Supply (supplied, suppliedName)
+import Noll.Compiler.Transform.Expression (mapMOverExpression)
 import Noll.Label (Label (..))
 import Noll.Language.Expression (Clause (..), Expression (..))
 import Noll.Language.Expression.Binding (Binding (..))
@@ -72,34 +73,6 @@ instance (Monoid a) => Expandable a o k (Binding Expression a (Type o k)) where
 instance (Monoid a) => Expandable a o k (Expression a (Type o k)) where
   expandPatterns =
     \case
-      EAnnotation a t e ->
-        EAnnotation a t <$> expandPatterns e
-      EApplication a t e1 es ->
-        EApplication a t <$> expandPatterns e1 <*> traverse expandPatterns es
-      EIf a t e1 e2 e3 ->
-        EIf a t <$> expandPatterns e1 <*> expandPatterns e2 <*> expandPatterns e3
-      ERecord a t d e ->
-        ERecord a t <$> traverse expandPatterns d <*> traverse expandPatterns e
-      EListCons a t e1 e2 ->
-        EListCons a t <$> expandPatterns e1 <*> expandPatterns e2
-      EListLiteral a t es ->
-        EListLiteral a t <$> traverse expandPatterns es
-      EMatch a t e cs ->
-        EMatch a t <$> expandPatterns e <*> pure cs
-      ESelect a (Label t name) e ->
-        ESelect a (Label t name) <$> expandPatterns e
-      EFold a t es cs e ->
-        EFold a t <$> traverse expandPatterns es <*> pure cs <*> traverse expandPatterns e
-      e@EUnaryOperator{} ->
-        pure e
-      e@EBinaryOperator{} ->
-        pure e
-      e@EVariable{} ->
-        pure e
-      e@EConstructor{} ->
-        pure e
-      e@ELiteral{} ->
-        pure e
       ELet a gs e1 -> do
         e2 <- expandPatterns e1
         (hs, ps) <- runWriterT (traverse expandPatterns gs)
@@ -112,6 +85,8 @@ instance (Monoid a) => Expandable a o k (Expression a (Type o k)) where
         e1 <- expandPatterns e
         (qs, ps) <- runWriterT (traverse expandPatterns ps)
         pure (ELambda a qs (foldr unrollMatch e1 ps))
+      e ->
+        mapMOverExpression expandPatterns e
 
 unrollMatch :: (Monoid a) => (Name, Pattern a (Type o k)) -> Expression a (Type o k) -> Expression a (Type o k)
 unrollMatch (name, p) e =
