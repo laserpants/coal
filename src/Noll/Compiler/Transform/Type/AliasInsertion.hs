ccad7818c8e84a75e62e239199dcc92f49f1f600
Johannes Hildén <johannes@knodd.se>
2025-01-24 10:23:53 +0100

    wip

--- src/Noll/Compiler/Transform/Type/AliasInsertion.hs
+++ src/Noll/Compiler/Transform/Type/AliasInsertion.hs
@@ -8,59 +8,62 @@ import Control.Monad.Reader (MonadReader)
 import Noll.Common.Environment (Environment)
 import Noll.Common.List1 (NonEmpty (..))
 import Noll.Language (
-  Module (..),
   Constant (..),
   Expression (..),
-  Pattern (..),
-  Object (..),
   Function (..),
   IndexedType,
+  Module (..),
   Object (..),
+  Pattern (..),
   Row (..),
   Trait (..),
   Type (..),
+  TypeIndex (..),
   Uses (..),
  )
 import Noll.Utils (Dictionary, Name)
 
 class AliasContext c where
-  expandAliases :: (MonadReader (Environment ([Name], IndexedType)) m) => c -> m c
+  insertAliases :: (MonadReader (Environment ([Name], IndexedType)) m) => c -> m c
 
 instance AliasContext () where
-  expandAliases _ = pure ()
+  insertAliases _ = pure ()
 
 instance (AliasContext c) => AliasContext [c] where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext c) => AliasContext (Dictionary c) where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext c) => AliasContext (NonEmpty c) where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext t) => AliasContext (Trait t) where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext t) => AliasContext (Uses t) where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext t) => AliasContext (Row o k t) where
-  expandAliases = traverse expandAliases
+  insertAliases = traverse insertAliases
 
 instance (AliasContext t) => AliasContext (Pattern a t) where
-  expandAliases = undefined
+  insertAliases = undefined
 
 instance (AliasContext t) => AliasContext (Expression a t) where
-  expandAliases = undefined
+  insertAliases = undefined
 
 instance (AliasContext t) => AliasContext (Module e a t) where
-  expandAliases = undefined
+  insertAliases = undefined
 
 instance (AliasContext (e a t), AliasContext t) => AliasContext (Function e a t) where
-  expandAliases = undefined
+  insertAliases = undefined
 
 instance (AliasContext (e a t), AliasContext t) => AliasContext (Constant e a t) where
-  expandAliases = undefined
+  insertAliases = undefined
 
 instance (AliasContext t) => AliasContext (Object a k t) where
-  expandAliases = undefined
+  insertAliases = undefined
+
+instance AliasContext (Type TypeIndex k) where
+  insertAliases = undefined
