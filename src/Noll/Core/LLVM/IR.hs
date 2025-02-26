8435f58f9dd1027df4904315df1c0494721b43fd
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-02-26 22:53:45 +0100

    wip

--- src/Noll/Core/LLVM/IR.hs
+++ src/Noll/Core/LLVM/IR.hs
@@ -2,8 +2,14 @@ module Noll.Core.LLVM.IR (
   module Noll.Core.LLVM.IRInstruction,
   module Noll.Core.LLVM.IRType,
   module Noll.Core.LLVM.IRValue,
+  module Noll.Core.LLVM.IRInstruction.Eval,
+  module Noll.Core.LLVM.IRInstruction.Interpreter,
+  module Noll.Core.LLVM.IRInstruction.TH,
 ) where
 
 import Noll.Core.LLVM.IRInstruction
+import Noll.Core.LLVM.IRInstruction.Eval
+import Noll.Core.LLVM.IRInstruction.Interpreter
+import Noll.Core.LLVM.IRInstruction.TH
 import Noll.Core.LLVM.IRType
 import Noll.Core.LLVM.IRValue
