39448e3ce057ee0d8219058627dacc1e03c4fc47
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-03-05 18:39:03 +0100

    wip

--- src/Noll/Core/LLVM/IRInstruction/Eval/Closure/Finalize.hs
+++ src/Noll/Core/LLVM/IRInstruction/Eval/Closure/Finalize.hs
@@ -4,15 +4,13 @@
 
 module Noll.Core.LLVM.IRInstruction.Eval.Closure.Finalize where -- (irClosureFinalize) where
 
-import Control.Monad (liftM, void)
-import Control.Monad.Free (MonadFree)
-import Control.Monad.State (MonadState, StateT, evalStateT, get, modify)
-import Noll.Core.LLVM.IRInstruction (IRInstr, IRInstrOp)
+import Control.Monad.State (evalStateT, get, modify)
+import Noll.Core.LLVM.IRInstruction (IRInstr)
 import Noll.Core.LLVM.IRInstruction.TH
 import Noll.Core.LLVM.IRType (IRType (..))
 import Noll.Core.LLVM.IRType.Syntax (i1, i32, i8Ptr, opaqueIRSignature, ptr, struct)
 import Noll.Core.LLVM.IRValue (IRValue (..))
-import Noll.Utils (Name, forM, forM_)
+import Noll.Utils (forM, forM_)
 import TextShow (showt)
 
 s applied = struct (i32 : replicate (3 + applied) i8Ptr)
@@ -27,32 +25,32 @@ irClosureFinalize n argF argN argAs = do
   iComment "Target function"
   iComment ""
   r3 <- iLoad i8Ptr r2
-  xs <- forM [1 .. n] $ \m -> do
-    rm <- iGep (xx n) r1 (I32 0) (I32 (3 + fromIntegral m))
-    iComment ""
-    iComment ("Applied arg #" <> showt m)
-    iComment ""
-    iLoad i8Ptr rm
+  rs <- forM [1 .. n] $
+    \m -> do
+      rm <- iGep (xx n) r1 (I32 0) (I32 (3 + fromIntegral m))
+      iComment ""
+      iComment ("Applied arg #" <> showt m)
+      iComment ""
+      iLoad i8Ptr rm
   flip evalStateT [] $
-    forM_ [1 .. 3 :: Int] $
+    forM_ [1 .. 3] $
       \m -> do
         r4 <- iGep1 i8Ptr argAs (I32 (fromIntegral (m - 1)))
         iComment ""
         iComment ("Extra arg #" <> showt m)
         iComment ""
-        rx <- iLoad i8Ptr r4
-        r5 <- iCmpSGt i1 argN (I32 (fromIntegral m))
+        r5 <- iLoad i8Ptr r4
+        r6 <- iCmpSGt i1 argN (I32 (fromIntegral m))
         labelGt <- iLabel "gt"
         labelEq <- iLabel "eq"
-        iBr r5 [labelGt, labelEq]
-        modify (\ys -> ys <> [rx])
-        ys <- get
+        iBr r6 [labelGt, labelEq]
+        modify (<> [r5])
+        qs <- get
         iBlock_ labelEq $ do
-          r6 <- iBCast r3 (opaqueIRSignature (n + m))
-          r7 <- iCall i8Ptr r6 (xs <> ys)
-          iRet i8Ptr r7
-          pure r7
+          r7 <- iBCast r3 (opaqueIRSignature (n + m))
+          r8 <- iCall i8Ptr r7 (rs <> qs)
+          iRet i8Ptr r8
+          pure r8
         iBlock_ labelGt $ do
-          pure r5
-        pure r5
+          pure r6
   iRet i8Ptr Null
