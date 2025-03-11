d85aaa3826017f40e6d0d4cab155326d52441612
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-03-11 09:35:07 +0100

    wip

--- src/Noll/Core/LLVM/IRInstruction/Interpreter/Artifact.hs
+++ src/Noll/Core/LLVM/IRInstruction/Interpreter/Artifact.hs
@@ -5,12 +5,10 @@ module Noll.Core.LLVM.IRInstruction.Interpreter.Artifact (interpretArtifact) whe
 
 import Data.Text.Encoding (encodeUtf8)
 import Noll.Core.LLVM.IRConstruct (IRConstruct (..), IRLinkage (..))
-import Noll.Core.LLVM.IREval.Closure (structType)
 import Noll.Core.LLVM.IRInstruction.Interpreter (IRInterpreter (..), IRLine (..))
 import Noll.Core.LLVM.IRInstruction.Interpreter.State
 import Noll.Core.LLVM.IRType.Syntax (i8Ptr)
 import Noll.Core.LLVM.IRValue (IRValue (..))
-import TextShow (showt)
 
 interpretArtifact :: IRInterpreterArtifact -> IRInterpreter [IRConstruct [IRLine]]
 interpretArtifact =
