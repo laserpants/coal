4dd32b18a81bc1ad4c6347d9f0c6976714772aaf
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-04-29 23:17:36 +0200

    wip

--- src/Noll/Compiler/Lowpass/TranslateExpression.hs
+++ src/Noll/Compiler/Lowpass/TranslateExpression.hs
@@ -6,6 +6,7 @@ module Noll.Compiler.Lowpass.TranslateExpression where
 import Lang.Label (Label (..))
 import Noll.Compiler.Lowpass.TranslateType (translateType)
 import Noll.Language.Expression
+import Noll.Language.Pattern
 import Noll.Language.Primitive
 import Noll.Language.Type
 
@@ -38,12 +39,16 @@ translateExpression =
   \case
     EAnnotation _ _ e ->
       translateExpression e
-    --    EApplication a t (Expression a t) (List1 (Expression a t))
-    --    ELambda a (List1 (Pattern a t)) (Expression a t)
-    --    ELet a (List1 (Binding Expression a t)) (Expression a t)
-    --    ERecursiveLet a (Pattern a t) (Expression a t) (Expression a t)
-    EVariable _ (Label t name) ->
-      Lowpass.var (Label (translateType t) name)
+    EApplication a t e es ->
+      undefined
+    ELambda _ ps e ->
+      Lowpass.lam (translatePattern <$> ps) (translateExpression e)
+    ELet a vs e ->
+      undefined
+    ERecursiveLet a p e1 e2 ->
+      undefined
+    EVariable _ ll ->
+      Lowpass.var (translateLabel ll)
     EConstructor _ (Label t name) ->
       undefined
     ELiteral _ p ->
@@ -53,25 +58,44 @@ translateExpression =
         (translateExpression e1)
         (translateExpression e2)
         (translateExpression e3)
-    --    EUnaryOperator a t UnaryOperator
-    --    EBinaryOperator a t BinaryOperator
-    --    ERecord a t (Dictionary (Expression a t)) (Maybe (Expression a t))
+    EUnaryOperator a t op ->
+      undefined
+    EBinaryOperator a t op ->
+      undefined
+    ERecord a t d me ->
+      undefined
     EListCons a t e1 e2 ->
       Lowpass.cons (translateExpression e1) (translateExpression e2)
-    --    EListLiteral a t [Expression a t]
+    EListLiteral a t es ->
+      undefined
     ETuple _ _ es ->
       Lowpass.tupleExpr (translateExpression <$> es)
     EMatch{} ->
       error "Implementation error"
     ECompiledMatch _ t e cs ->
-      Lowpass.match
-        (translateType t)
-        (translateExpression e)
-        undefined
+      Lowpass.match (translateType t) (translateExpression e) (translateClause <$> cs)
     EFold _ _ _ _ (Just e) ->
       translateExpression e
+    ESelect _ ll e ->
+      error "TODO"
+    EFocus name0 (Label t1 name1) (Label t2 name2) e1 e2 ->
+      Lowpass.sel (Lowpass.Focus undefined undefined undefined) undefined undefined
+    EDictionaryLambda a ts e ->
+      undefined
+    EDictionaryApplication a t e ts es ->
+      undefined
 
---    ESelect a (Label t) (Expression a t)
---    EFocus Name (Label t) (Label t) (Expression a t) (Expression a t)
---    EDictionaryLambda a (List1 (Trait t)) (Expression a t)
---    EDictionaryApplication a t (Expression a t) (List1 (Trait t)) [Expression a t]
+translatePattern :: Pattern a (Type o k) -> Label Lowpass.Type
+translatePattern =
+  \case
+    PVariable a (Label t name) ->
+      Label (translateType t) name
+
+translateClause :: CompiledClause a (Type o k) -> Lowpass.Clause Lowpass.Type (Lowpass.Expr Lowpass.Type)
+translateClause =
+  \case
+    ECompiledClause lls e ->
+      Lowpass.Clause (translateLabel <$> lls) (translateExpression e)
+
+translateLabel :: Label (Type o k) -> Label Lowpass.Type
+translateLabel (Label t name) = Label (translateType t) name
