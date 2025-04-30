1eeef56200b05686ab9bc7066fa1d21bfcaca4b7
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-04-30 07:15:17 +0200

    wip

--- src/Noll/Compiler/Lowpass/TranslateExpression.hs
+++ src/Noll/Compiler/Lowpass/TranslateExpression.hs
@@ -3,6 +3,7 @@
 
 module Noll.Compiler.Lowpass.TranslateExpression where
 
+import Lang.Common.List1 (NonEmpty (..))
 import Lang.Label (Label (..))
 import Noll.Compiler.Lowpass.TranslateType (translateType)
 import Noll.Language.Expression
@@ -46,12 +47,14 @@ translateExpression =
       Lowpass.lam (translatePattern <$> ps) (translateExpression e)
     ELet _ vs e ->
       Lowpass.let_ (translateBinding <$> vs) (translateExpression e)
-    ERecursiveLet _ p e1 e2 ->
-      Lowpass.let_ undefined undefined
+    ERecursiveLet _ (PVariable _ ll) e1 e2 ->
+      Lowpass.let_
+        (Lowpass.Binding (translateLabel ll) (translateExpression e1) :| [])
+        (translateExpression e2)
     EVariable _ ll ->
       Lowpass.var (translateLabel ll)
-    EConstructor _ (Label t name) ->
-      undefined
+    EConstructor _ ll ->
+      Lowpass.var (translateLabel ll)
     ELiteral _ p ->
       Lowpass.lit (translatePrimitive p)
     EIf _ _ e1 e2 e3 ->
@@ -64,8 +67,8 @@ translateExpression =
       undefined
     EListCons a t e1 e2 ->
       Lowpass.cons (translateExpression e1) (translateExpression e2)
-    EListLiteral a t es ->
-      undefined
+    EListLiteral a t (e : es) ->
+      translateExpression (foldr (EListCons a t) e es)
     ETuple _ _ es ->
       Lowpass.tupleExpr (translateExpression <$> es)
     EMatch{} ->
@@ -76,8 +79,11 @@ translateExpression =
       translateExpression e
     ESelect _ ll e ->
       error "TODO"
-    EFocus name0 (Label t1 name1) (Label t2 name2) e1 e2 ->
-      Lowpass.sel (Lowpass.Focus undefined undefined undefined) undefined undefined
+    EFocus name0 ll1 ll2 e1 e2 ->
+      Lowpass.sel
+        (Lowpass.Focus name0 (translateLabel ll1) (translateLabel ll2))
+        (translateExpression e1)
+        (translateExpression e2)
     EDictionaryLambda a ts e ->
       undefined
     EDictionaryApplication a t e ts es ->
@@ -86,8 +92,8 @@ translateExpression =
 translateBinding :: Binding Expression a (Type o k) -> Lowpass.Binding Lowpass.Type (Lowpass.Expr Lowpass.Type)
 translateBinding =
   \case
-    BPattern _ (PVariable _ (Label t name)) e ->
-      Lowpass.Binding (Label (translateType t) name) (translateExpression e)
+    BPattern _ (PVariable _ ll) e ->
+      Lowpass.Binding (translateLabel ll) (translateExpression e)
 
 translatePattern :: Pattern a (Type o k) -> Label Lowpass.Type
 translatePattern =
