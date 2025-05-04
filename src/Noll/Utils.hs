1dfe197f856c993a914ffd16d3aab4dddd7a7d63
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-05-04 08:04:10 +0200

    wip

--- src/Noll/Utils.hs
+++ src/Noll/Utils.hs
@@ -7,6 +7,10 @@ import Numeric (showHex)
 
 import qualified Data.Text as Text
 
+{-# INLINE inCharRange #-}
+inCharRange :: Int -> (Char, Char) -> Bool
+inCharRange n (a, b) = n >= ord a && n <= ord b
+
 lexOrderRank :: Text -> Int
 lexOrderRank text
   | Text.null text =
@@ -17,12 +21,10 @@ lexOrderRank text
   f :: Char -> (Int, Int) -> (Int, Int)
   f c (m, n) = (m + 1, n + (36 ^ m) + g (ord c))
   g n
-    | not ((n >= 97 && n <= 122) || (n >= 48 && n <= 57)) =
+    | not (n `inCharRange` ('a', 'z') || n `inCharRange` ('0', '9')) =
         error "Invalid character"
-    | n >= 97 =
-        n - 97
-    | otherwise =
-        n - 22
+    | n >= ord 'a' = n - ord 'a'
+    | otherwise = n - 22
 
 hashed :: (Hashable a) => a -> Text
 hashed t = Text.pack (showHex (fromIntegral (hash t) :: Word) "")
