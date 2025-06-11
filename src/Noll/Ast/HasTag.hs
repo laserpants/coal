c761a6d8e6117d2f215d1726acd0cfbcadedae58
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-06-11 07:35:52 +0200

    wip

--- src/Noll/Ast/HasTag.hs
+++ src/Noll/Ast/HasTag.hs
@@ -1,19 +1,22 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE StrictData #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE StrictData #-}
 
-module Noll.Ast.HasTag (HasTag (..)) where
+-- TODO: remove?
+module Noll.Ast.HasTag where
 
-import Data.Data (Data)
-import Data.Generics.Uniplate.Data (universeBi)
-import Noll.Language.Expression (Expression (..))
-import Noll.Language.Pattern (Pattern (..))
-
-class HasTag t a where
-  tag :: t -> a
-
-instance (Data a, Data t) => HasTag (Expression a t) a where
-  tag = head . universeBi
-
-instance (Data a, Data t) => HasTag (Pattern a t) a where
-  tag = head . universeBi
+--module Noll.Ast.HasTag (HasTag (..)) where
+--
+--import Data.Data (Data)
+--import Data.Generics.Uniplate.Data (universeBi)
+--import Noll.Language.Expression (Expression (..))
+--import Noll.Language.Pattern (Pattern (..))
+--
+--class HasTag t a where
+--  tag :: t -> a
+--
+--instance (Data a, Data t) => HasTag (Expression a t) a where
+--  tag = head . universeBi
+--
+--instance (Data a, Data t) => HasTag (Pattern a t) a where
+--  tag = head . universeBi
