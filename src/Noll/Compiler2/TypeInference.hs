ef6f794a4b8371af4f8d2ec09636c70cd862fcfb
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-06-29 08:24:27 +0200

    wip

--- src/Noll/Compiler2/TypeInference.hs
+++ src/Noll/Compiler2/TypeInference.hs
@@ -5,9 +5,18 @@
 module Noll.Compiler2.TypeInference where
 
 import Noll.Compiler2.Internal
+import Noll.Language
+import Noll.Module (Definition (..))
+import Noll.SystemF
 
+type CompilerAssumption = Assumption IndexedType
+
+type CompilerConstraint a = Constraint (InferenceRule Kind a) TypeIndex Kind IndexedType
+
+typeDefinitionsC :: [Definition a k IndexedType] -> Compiler2T m ([Definition a Kind IndexedType], [CompilerAssumption])
 typeDefinitionsC = undefined
 
+typeDefinitionC :: Definition a k IndexedType -> Compiler2T m (Definition a Kind IndexedType)
 typeDefinitionC =
   \case
     _ ->
