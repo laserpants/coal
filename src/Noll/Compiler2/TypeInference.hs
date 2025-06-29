7f3b36121a14f2202165c19bd35f2b51cc2a4a2e
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-06-29 08:12:09 +0200

    wip

--- src/Noll/Compiler2/TypeInference.hs
+++ src/Noll/Compiler2/TypeInference.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StrictData #-}
 
 module Noll.Compiler2.TypeInference where
 
 typeDefinitionsC = undefined
+
+typeDefinitionC = 
+  \case
+    _ ->
+      undefined
