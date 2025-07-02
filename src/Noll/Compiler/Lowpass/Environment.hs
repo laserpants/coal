7d50d034a1b823e9b29255ed3ef716ea71c3875c
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-07-02 17:15:17 +0200

    wip

--- src/Noll/Compiler/Lowpass/Environment.hs
+++ src/Noll/Compiler/Lowpass/Environment.hs
@@ -46,7 +46,6 @@ qualifyName :: (MonadReader TranslateEnvironment m) => Name -> m Name
 qualifyName name = do
   TranslateEnvironment{..} <- ask
   if Text.head name == '$' || Set.member name translateEnvironmentLocalNames
-    --  if Set.member name translateEnvironmentLocalNames
     then pure name
     else case Environment.lookup name translateEnvironmentQualifiedNames of
       Just qname ->
