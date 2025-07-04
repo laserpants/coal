11177cae3d8f1324a4162d41aaf45d8261abb95a
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-07-05 00:22:51 +0300

    wip

--- test/Noll/Set7/Test01.hs
+++ test/Noll/Set7/Test01.hs
@@ -10,7 +10,7 @@ import Noll.Module (Constant (..), Definition (..), Function (..), Module (..), 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Data.Text as Text
-import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Encoding as Text
 import qualified Noll.Module as Module
 
 prog7_01 :: [Module () o ()]
@@ -36,7 +36,7 @@ moduleMain =
                 ()
                 ()
                 (EVariable () (Label () "trace_string"))
-                ( ELiteral () (LString (TE.encodeUtf8 (Text.pack "Hello 🤖, world!")))
+                ( ELiteral () (LString (Text.encodeUtf8 (Text.pack "Hello 🤖, world!")))
                     :| []
                 )
             )
