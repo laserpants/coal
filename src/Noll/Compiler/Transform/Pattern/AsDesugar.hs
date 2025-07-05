054a1f5bfce4dfe23485fdfe0e816716a4ebdf3c
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-07-05 19:20:07 +0300

    wip

--- src/Noll/Compiler/Transform/Pattern/AsDesugar.hs
+++ src/Noll/Compiler/Transform/Pattern/AsDesugar.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StrictData #-}
 
 module Noll.Compiler.Transform.Pattern.AsDesugar where
 
+import Lang.Common.List1 (List1, NonEmpty (..))
 import Noll.Language (Choice (..), Clause (..), Expression (..), Pattern (..))
 
 class AsDesugarContext e where
   desugarAsPatterns :: e -> e
+
+instance (AsDesugarContext e) => AsDesugarContext [e] where
+  desugarAsPatterns = fmap desugarAsPatterns
+
+instance (AsDesugarContext e) => AsDesugarContext (NonEmpty e) where
+  desugarAsPatterns = fmap desugarAsPatterns
 
 instance AsDesugarContext (Clause a t) where
   desugarAsPatterns =
@@ -14,3 +22,13 @@ instance AsDesugarContext (Clause a t) where
       EClause a p cs ->
         undefined
 
+instance AsDesugarContext (Choice Expression a t) where
+  desugarAsPatterns =
+    \case
+      CPlain a gs e ->
+        undefined
+      CLambda{} ->
+        error "TODO"
+
+instance AsDesugarContext (Expression a t) where
+  desugarAsPatterns = undefined
