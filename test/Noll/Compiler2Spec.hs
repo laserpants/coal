edcfb09965ebe34f01fcff2b4723bc14cad0ff7b
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-07-18 13:31:34 +0300

    wip

--- test/Noll/Compiler2Spec.hs
+++ test/Noll/Compiler2Spec.hs
@@ -47,6 +47,7 @@ import qualified Noll.Set20.Test01
 import qualified Noll.Set21.Test01
 import qualified Noll.Set22.Test01
 import qualified Noll.Set23.Test01
+import qualified Noll.Set24.Test01
 import qualified Noll.Set7.Test01
 import qualified Noll.Set8.Test01
 import qualified Noll.Set9.Test01
@@ -702,6 +703,49 @@ abc28 = fst (runIdentity (runCompiler2T compiler2TestEnvironment prog))
 
 abc29 :: IO ()
 abc29 = Lowpass.testModules =<< Lowpass.compileModules [moduleCore1, abc28]
+
+abc30 :: Lowpass.Module Lowpass.Type Name (Lowpass.Expr Lowpass.Type)
+abc30 = fst (runIdentity (runCompiler2T compiler2TestEnvironment prog))
+ where
+  prog = do
+    insertNamesC
+      [
+        ( "flatten"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (tree0 `TArrow` TIntrinsic (IList (TVariable (TypeIndex KType 0))))
+        )
+      ,
+        ( "from_list"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TIntrinsic (IList (TVariable (TypeIndex KType 0))) `TArrow` tree0)
+        )
+      ,
+        ( "compare"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TVariable (TypeIndex KType 0) `TArrow` TVariable (TypeIndex KType 0) `TArrow` TConstructor KType "Ordering")
+        )
+      ,
+        ( "greater_than"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TVariable (TypeIndex KType 0) `TArrow` TVariable (TypeIndex KType 0) `TArrow` TIntrinsic IBool)
+        )
+      ,
+        ( "less_than_or_equal_to"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TVariable (TypeIndex KType 0) `TArrow` TVariable (TypeIndex KType 0) `TArrow` TIntrinsic IBool)
+        )
+      ,
+        ( "operator__not"
+        , Forall mempty [] (TIntrinsic IBool `TArrow` TIntrinsic IBool)
+        )
+      ,
+        ( "in_range"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TIntrinsic (IRecord (TRow (RExtend "max" (TVariable (TypeIndex KType 0)) (RExtend "min" (TVariable (TypeIndex KType 0)) RNil)))) `TArrow` TVariable (TypeIndex KType 0) `TArrow` TIntrinsic IBool)
+        )
+      ,
+        ( "from_int32"
+        , Forall (Set.fromList [TypeIndex KType 0]) [] (TIntrinsic IInt32 `TArrow` TVariable (TypeIndex KType 0))
+        )
+      ]
+    compileModule_ Noll.Set24.Test01.moduleMain
+
+abc31 :: IO ()
+abc31 = Lowpass.testModules =<< Lowpass.compileModules [moduleCore1, abc30]
 
 -- abc25 =
 --  abc22
