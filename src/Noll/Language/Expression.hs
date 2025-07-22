a1fe1d0a1ffa1678cbb8e604b02f77e607c4aba3
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-07-22 11:49:49 +0300

    wip

--- src/Noll/Language/Expression.hs
+++ src/Noll/Language/Expression.hs
@@ -5,7 +5,11 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE StrictData #-}
 
-module Noll.Language.Expression (Expression (..), Clause (..), CompiledClause (..)) where
+module Noll.Language.Expression (
+  Expression (..),
+  Clause (..),
+  CompiledClause (..),
+) where
 
 import Data.Data (Data, Typeable)
 import Data.Generics.Uniplate.Data (universeBi)
