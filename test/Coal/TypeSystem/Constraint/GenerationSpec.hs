308b6e1c7dd341663b50ab1d7fa2cc6066ef60d1
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-13 17:59:10 +0300

    wip

--- test/Coal/TypeSystem/Constraint/GenerationSpec.hs
+++ test/Coal/TypeSystem/Constraint/GenerationSpec.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Coal.TypeSystem.Constraint.GenerationSpec where 
+module Coal.TypeSystem.Constraint.GenerationSpec where
 
+import Coal.Common.Label (Label (..))
 import Coal.Language
 import Coal.TypeSystem.Constraint.Generation
-import Coal.Common.Label (Label (..))
 
 fixture1 :: Expression () IndexedType
 fixture1 =
   EConstructor () (Label (TConstructor KType "Color") "Blue")
 
 collectConstraintsSpec = a
-  where
-    a = runConstraintsGenStack (freshIdIn fixture1) ctx (collectConstraints fixture1)
-    ctx = ConstraintsGenContext
+ where
+  a = runConstraintsGenStack (freshIdIn fixture1) ctx (collectConstraints fixture1)
+  ctx =
+    ConstraintsGenContext
       { constraintsGenContextMonomorphicSet = mempty
       , constraintsGenContextDataConstructorEnv = mempty
       , constraintsGenContextCodataAccessorEnv = mempty
