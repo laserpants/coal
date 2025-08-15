0c774ca17ae44fb90da29a8d227452c91959cac0
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-15 21:02:05 +0300

    wip

--- test/Coal/TypeSystem/Constraint/Generation/ELambdaConstraintsSpec.hs
+++ test/Coal/TypeSystem/Constraint/Generation/ELambdaConstraintsSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Coal.TypeSystem.Constraint.Generation.ELambdaConstraintsSpec where
@@ -11,6 +12,7 @@ import Coal.TypeSystem.Constraint.Generation.InferenceRule
 import Data.Either (lefts, rights)
 
 import qualified Coal.Common.Environment as Environment
+import qualified Data.Set as Set
 
 fixture1 :: Expression () IndexedType
 fixture1 =
@@ -47,8 +49,8 @@ fixture2 =
     (PVariable () (Label (TVariable (TypeIndex KType 0)) "x") :| [])
     (EVariable () (Label (TVariable (TypeIndex KType 1)) "y"))
 
--- collectELambdaConstraintsSpec1 :: Bool
-collectELambdaConstraintsSpec2 = (ms, outs)
+collectELambdaConstraintsSpec2 :: Bool
+collectELambdaConstraintsSpec2 = null outs
  where
   (ms, outs) = evalConstraintsGenStack (freshIdIn expr) ctx (collectConstraints expr)
   expr = fixture2
@@ -62,3 +64,54 @@ collectELambdaConstraintsSpec2 = (ms, outs)
 
 collectELambdaConstraintsSpec3 :: Bool
 collectELambdaConstraintsSpec3 = 2 == freshIdIn fixture2
+
+fixture3 :: Expression () IndexedType
+fixture3 =
+  -- fn(x) => let y = x in y
+  ELambda
+    ()
+    (PVariable () (Label (TVariable (TypeIndex KType 0)) "x") :| [])
+    ( ELet
+        ()
+        ( BPattern
+            ()
+            (PVariable () (Label (TVariable (TypeIndex KType 1)) "y"))
+            (EVariable () (Label (TVariable (TypeIndex KType 2)) "x"))
+            :| []
+        )
+        (EVariable () (Label (TVariable (TypeIndex KType 3)) "y"))
+    )
+
+muteConstraint :: Constraint c o k t -> Constraint () o k t
+muteConstraint =
+  \case
+    Equality _ ts ->
+      Equality () ts
+    Implicit _ t1 t2 m ->
+      Implicit () t1 t2 m
+    Explicit _ t s ->
+      Explicit () t s
+
+constraint2 :: Constraint () TypeIndex Kind IndexedType
+constraint2 = Equality () [TVariable (TypeIndex KType 1), TVariable (TypeIndex KType 2)]
+
+constraint3 :: Constraint () TypeIndex Kind IndexedType
+constraint3 = Implicit () (TVariable (TypeIndex KType 3)) (TVariable (TypeIndex KType 1)) (Monomorphic (Set.fromList [TypeIndex KType 0]))
+
+constraint4 :: Constraint () TypeIndex Kind IndexedType
+constraint4 = Equality () [TVariable (TypeIndex KType 2), TVariable (TypeIndex KType 0)]
+
+collectELambdaConstraintsSpec4 :: Bool
+collectELambdaConstraintsSpec4 =
+  constraint2 `elem` constraints && constraint3 `elem` constraints && constraint4 `elem` constraints
+ where
+  constraints = muteConstraint <$> rights outs
+  (ms, outs) = evalConstraintsGenStack (freshIdIn expr) ctx (collectConstraints expr)
+  expr = fixture3
+  ctx =
+    ConstraintsGenContext
+      { constraintsGenContextMonomorphicSet = mempty
+      , constraintsGenContextDataConstructorEnv = mempty
+      , constraintsGenContextCodataAccessorEnv = mempty
+      , constraintsGenContextTypeConstructorEnv = mempty
+      }
