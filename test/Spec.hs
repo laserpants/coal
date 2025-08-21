9ca3088ac2e03b9a9b09e1011d22735c3f39c250
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-21 09:28:21 +0300

    wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -180,8 +180,6 @@ spec = do
   print (x == Right "Covfefe\n")
   x <- main72
   print (x == Right "true\n")
-  x <- main73
-  print (x == Right "false\n")
 
 runTestFiles :: [String] -> IO (Either CompilerError Text)
 runTestFiles files = do
@@ -620,12 +618,6 @@ main72 :: IO (Either CompilerError Text)
 main72 = do
   runTestFiles
     [ "./test/Coal/examples/72/Main.coal"
-    ]
-
-main73 :: IO (Either CompilerError Text)
-main73 = do
-  runTestFiles
-    [ "./test/Coal/examples/73/Main.coal"
     ]
 
 compileFiles :: [String] -> IO (Either CompilerError ())
