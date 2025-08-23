b17a1e312cea449b81dd3c58c9c1e5fa71306bff
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-23 23:43:26 +0300

    wip

--- test/Coal/TypeSystem/UnificationSpec.hs
+++ test/Coal/TypeSystem/UnificationSpec.hs
@@ -7,6 +7,7 @@ import Coal.Language
 import Coal.TypeSystem.Substitution
 import Coal.TypeSystem.Unification
 
+import qualified Coal.Common.List1 as List1
 import qualified Coal.TypeSystem.Substitution as Substitution
 
 data UnificationSpecTestCase t = UnificationSpecTestCase t t (Either UnificationError Substitution)
@@ -72,13 +73,13 @@ testCases =
       (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32)
       (Right mempty)
   , -- bool -> int32 ~ int32 -> int32
-    -- <does not unify>
+    -- ECannotUnify
     UnificationSpecTestCase
       (TIntrinsic IBool `TArrow` TIntrinsic IInt32)
       (TIntrinsic IInt32 `TArrow` TIntrinsic IInt32)
       (Left ECannotUnify)
   , -- bool ~ int32
-    -- <does not unify>
+    -- ECannotUnify
     UnificationSpecTestCase
       (TIntrinsic IBool)
       (TIntrinsic IInt32)
@@ -120,7 +121,7 @@ testCases =
       (TRow (RExtend "id" (TIntrinsic IInt32) (RExtend "name" (TIntrinsic IString) RNil)))
       (Right mempty)
   , -- { name : string, id : int32 } ~ { name : int32, id : int32 }
-    -- <does not unify>
+    -- ECannotUnify
     UnificationSpecTestCase
       (TRow (RExtend "name" (TIntrinsic IString) (RExtend "id" (TIntrinsic IInt32) RNil)))
       (TRow (RExtend "name" (TIntrinsic IInt32) (RExtend "id" (TIntrinsic IInt32) RNil)))
@@ -141,6 +142,165 @@ testCases =
               , TRow RNil
               )
             ]
+      )
+  , -- '0:Type ~ '0:Row
+    -- EKindMismatch
+    UnificationSpecTestCase
+      (TVariable (TypeIndex KType 0))
+      (TVariable (TypeIndex KRow 0))
+      (Left EKindMismatch)
+  , -- '0 ~ '0 -> int32
+    -- EInfiniteType
+    UnificationSpecTestCase
+      (TVariable (TypeIndex KType 0))
+      (TArrow (TVariable (TypeIndex KType 0)) (TIntrinsic IInt32))
+      (Left EInfiniteType)
+  , -- '0:Row ~ { x : int32 | '0 }
+    -- EInfiniteType
+    UnificationSpecTestCase
+      (TVariable (TypeIndex KRow 0))
+      (TRow (RExtend "x" (TIntrinsic IInt32) (RVariable (TypeIndex KRow 0))))
+      (Left EInfiniteType)
+  , -- string ~ int32
+    -- ECannotUnify
+    UnificationSpecTestCase
+      (TIntrinsic IString)
+      (TIntrinsic IInt32)
+      (Left ECannotUnify)
+  , -- record { x : int32 } ~ record { x : int32 }
+    -- Substitution []
+    UnificationSpecTestCase
+      (TIntrinsic (IRecord (TRow (RExtend "x" (TIntrinsic IInt32) RNil))))
+      (TIntrinsic (IRecord (TRow (RExtend "x" (TIntrinsic IInt32) RNil))))
+      (Right mempty)
+  , -- int32 -> string ~ int32 -> int32
+    -- ECannotUnify
+    UnificationSpecTestCase
+      (TArrow (TIntrinsic IInt32) (TIntrinsic IString))
+      (TArrow (TIntrinsic IInt32) (TIntrinsic IInt32))
+      (Left ECannotUnify)
+  , -- '0 -> ('1 -> int32) ~ bool -> (string -> int32)
+    -- Substitution [ 0 :=> bool, 1 :=> string ]
+    UnificationSpecTestCase
+      ( TArrow
+          (TVariable (TypeIndex KType 0))
+          (TArrow (TVariable (TypeIndex KType 1)) (TIntrinsic IInt32))
+      )
+      ( TArrow
+          (TIntrinsic IBool)
+          (TArrow (TIntrinsic IString) (TIntrinsic IInt32))
+      )
+      ( Right $
+          Substitution.fromList
+            [ (0, TIntrinsic IBool)
+            , (1, TIntrinsic IString)
+            ]
+      )
+  , -- { x : int32, y : string } ~ { y : string, x : int32 }
+    -- Substitution []
+    UnificationSpecTestCase
+      ( TRow
+          ( RExtend
+              "x"
+              (TIntrinsic IInt32)
+              (RExtend "y" (TIntrinsic IString) RNil)
+          )
+      )
+      ( TRow
+          ( RExtend
+              "y"
+              (TIntrinsic IString)
+              (RExtend "x" (TIntrinsic IInt32) RNil)
+          )
+      )
+      (Right mempty)
+  , -- { x : int32 | '0 } ~ { x : int32, y : string }
+    -- Substitution [ 0 :=> { y : string } ]
+    UnificationSpecTestCase
+      (TRow (RExtend "x" (TIntrinsic IInt32) (RVariable (TypeIndex KRow 0))))
+      ( TRow
+          ( RExtend
+              "x"
+              (TIntrinsic IInt32)
+              (RExtend "y" (TIntrinsic IString) RNil)
+          )
+      )
+      ( Right $
+          Substitution.fromList
+            [(0, TRow (RExtend "y" (TIntrinsic IString) RNil))]
+      )
+  , -- { x : int32 } ~ { x : string }
+    -- ECannotUnify
+    UnificationSpecTestCase
+      (TRow (RExtend "x" (TIntrinsic IInt32) RNil))
+      (TRow (RExtend "x" (TIntrinsic IString) RNil))
+      (Left ECannotUnify)
+  , -- List '0 ~ int32
+    -- ECannotUnify
+    UnificationSpecTestCase
+      ( TApplication
+          KType
+          (TConstructor KType "List")
+          (List1.singleton (TVariable (TypeIndex KType 0)))
+      )
+      (TIntrinsic IInt32)
+      (Left ECannotUnify)
+  , -- type alias T a = a
+    -- T int32 ~ int32
+    UnificationSpecTestCase
+      (TAlias "T" [TIntrinsic IInt32] (TIntrinsic IInt32))
+      (TIntrinsic IInt32)
+      (Right mempty)
+  , -- type alias T a = a
+    -- T int32 ~ string
+    -- ECannotUnify
+    UnificationSpecTestCase
+      (TAlias "T" [TIntrinsic IInt32] (TIntrinsic IInt32))
+      (TIntrinsic IString)
+      (Left ECannotUnify)
+  , -- type alias Id a = a
+    -- Id '0 ~ int32
+    -- Substitution [ 0 :=> int32 ]
+    UnificationSpecTestCase
+      (TAlias "Id" [TVariable (TypeIndex KType 0)] (TVariable (TypeIndex KType 0)))
+      (TIntrinsic IInt32)
+      ( Right $
+          Substitution.fromList
+            [(0, TIntrinsic IInt32)]
+      )
+  , -- type alias Pair a b = (a -> b)
+    -- Pair '0 '1 ~ (int32 -> string)
+    -- Substitution [ 0 :=> int32, 1 :=> string ]
+    UnificationSpecTestCase
+      ( TAlias
+          "Pair"
+          [TVariable (TypeIndex KType 0), TVariable (TypeIndex KType 1)]
+          (TArrow (TVariable (TypeIndex KType 0)) (TVariable (TypeIndex KType 1)))
+      )
+      (TArrow (TIntrinsic IInt32) (TIntrinsic IString))
+      ( Right $
+          Substitution.fromList
+            [ (0, TIntrinsic IInt32)
+            , (1, TIntrinsic IString)
+            ]
+      )
+  , -- '0 ~ '1, '1 ~ int32
+    -- Substitution [ 0 :=> int32, 1 :=> int32 ]
+    UnificationSpecTestCase
+      (TVariable (TypeIndex KType 0))
+      (TVariable (TypeIndex KType 1))
+      ( Right $
+          Substitution.fromList
+            [(0, TVariable (TypeIndex KType 1))]
+      )
+  , -- '0 ~ '1 -> '1, '1 ~ int32
+    -- Substitution [ 1 :=> int32, 0 :=> (int32 -> int32) ]
+    UnificationSpecTestCase
+      (TVariable (TypeIndex KType 0))
+      (TArrow (TVariable (TypeIndex KType 1)) (TVariable (TypeIndex KType 1)))
+      ( Right $
+          Substitution.fromList
+            [(0, TArrow (TVariable (TypeIndex KType 1)) (TVariable (TypeIndex KType 1)))]
       )
   ]
 
