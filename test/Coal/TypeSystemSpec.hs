fca49e2b471c4dbc4167d42ff43c6602f396ebf5
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-29 15:19:08 +0300

    wip

---
+++ test/Coal/TypeSystemSpec.hs
@@ -0,0 +1,15 @@
+module Coal.TypeSystemSpec where
+
+import Coal.TypeSystem.SubstitutionSpec (substitutionSpec)
+import Coal.TypeSystem.TypeIndexedSpec
+import Coal.TypeSystem.UnificationSpec (unificationSpec)
+
+import Test.Hspec
+
+typeSystemSpec :: Spec
+typeSystemSpec =
+  describe "TypeSystem" $ do
+    unificationSpec
+    substitutionSpec
+    typeIndexedSpec
+    typeIndexedTrickySpec
