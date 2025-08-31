beba2a45975dee392e1f25bf3031007740e11c83
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-08-31 23:28:14 +0300

    wip

--- src/Coal/Compiler/Transform/Definition/Fold.hs
+++ src/Coal/Compiler/Transform/Definition/Fold.hs
@@ -1,3 +1,34 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
+
 module Coal.Compiler.Transform.Definition.Fold where
 
+import Coal.Common.Label (Label (..), labelName)
+import Coal.Common.Supply (suppliedName)
+import Coal.Compiler.Transform.Expression
+import Coal.Compiler.Transform.Flattening (flattenApplication)
+import Coal.Compiler.Transform.Tree (replace)
+import Coal.Language (Choice (..), Clause (..), Expression (..), Pattern (..))
+import Coal.Language.Module (Constant (..), Definition (..), Function (..), Module (..))
+import Control.Monad.RWS (RWS, runRWS)
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.State (MonadState)
+import Control.Monad.Writer (execWriter, tell)
+import Data.Data (Data)
+import Data.Generics.Uniplate.Data (transform, transformM)
+import Data.List.NonEmpty (NonEmpty (..))
+import Extra (Dictionary, Name, const2)
+
 -- TODO
+
+compileTopLevelFolds :: (Monad m) => Definition a k t -> m (Definition a k t)
+compileTopLevelFolds =
+  \case
+    DFold name cs _ ->
+      pure undefined
+    o ->
+      pure o
