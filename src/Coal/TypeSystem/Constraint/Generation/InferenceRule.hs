5dd6d2a49975fa44ef499730e9d24972fd246bba
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-09-13 13:30:41 +0200

    wip

--- src/Coal/TypeSystem/Constraint/Generation/InferenceRule.hs
+++ src/Coal/TypeSystem/Constraint/Generation/InferenceRule.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE StrictData #-}
 
--- FIXME
 module Coal.TypeSystem.Constraint.Generation.InferenceRule (InferenceRule (..)) where
 
 import Coal.Language (Kind (..), Scheme (..), Type (..), TypeIndex (..))
