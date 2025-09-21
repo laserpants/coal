0f57116f7e640a9bfe92baffebc691051f1df3f0
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-09-21 15:25:53 +0200

    wip

---
+++ src/Coal/Compiler/Pass/TypeImports.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
+
+module Coal.Compiler.Pass.TypeImports where
+
+import Coal.Compiler.Journal
+import Coal.Compiler.Pass
+import Coal.Compiler.Stack
+import Coal.Parser (ParserError)
+import Coal.Parser.Module (parseModule)
+import Control.Monad.Except (throwError)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Either (partitionEithers)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Extra (forM_)
+import Text.Megaparsec (runParser)
+
+typeImportsPass :: (MonadIO m) => Pass a m [ModuleBundle] [ModuleBundle]
+typeImportsPass =
+  Pass
+    { passName = "TypeImports"
+    , runPass = pass
+    }
+
+pass = undefined
