831134a4fc92efff57d736855d778cc68e366a5b
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-10-10 08:47:43 +0200

    wip

--- .readme/tex/compile.sh
+++ .readme/tex/compile.sh
@@ -6,8 +6,10 @@ pdflatex -output-directory=tmp fold.tex
 pdflatex -output-directory=tmp unfold.tex
 pdflatex -output-directory=tmp fold_unfold.tex
 pdflatex -output-directory=tmp product_coproduct.tex
+pdflatex -output-directory=tmp fixed_point.tex
 
 magick -density 300 tmp/fold.pdf -resize 50% -bordercolor white -border 10x10 png/fold.png
 magick -density 300 tmp/unfold.pdf -resize 50% -bordercolor white -border 10x10 png/unfold.png
 magick -density 300 tmp/fold_unfold.pdf -resize 50% -bordercolor white -border 10x10 png/fold_unfold.png
 magick -density 300 tmp/product_coproduct.pdf -resize 50% -bordercolor white -border 10x10 png/product_coproduct.png
+magick -density 300 tmp/fixed_point.pdf -resize 50% -bordercolor white -border 10x10 png/fixed_point.png
