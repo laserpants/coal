2640dcff9194dd200eb87705fbdc6b5fb9316983
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-10-10 15:02:26 +0200

    wip

--- .readme/tex/compile.sh
+++ .readme/tex/compile.sh
@@ -11,7 +11,7 @@ pdflatex -output-directory=tmp pattern_functor.tex
 
 magick -density 300 tmp/fold.pdf -resize 50% -bordercolor white -border 10x10 png/fold.png
 magick -density 300 tmp/unfold.pdf -resize 50% -bordercolor white -border 10x10 png/unfold.png
-magick -density 300 tmp/fold_unfold.pdf -resize 50% -bordercolor white -border 10x10 png/fold_unfold.png
+magick -density 300 tmp/fold_unfold.pdf -resize 30% -bordercolor white -border 10x10 png/fold_unfold.png
 magick -density 300 tmp/product_coproduct.pdf -resize 50% -bordercolor white -border 10x10 png/product_coproduct.png
 magick -density 300 tmp/fixed_point.pdf -resize 50% -bordercolor white -border 10x10 png/fixed_point.png
 magick -density 300 tmp/pattern_functor.pdf -resize 50% -bordercolor white -border 10x10 png/pattern_functor.png
