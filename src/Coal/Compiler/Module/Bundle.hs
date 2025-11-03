ebc3fec8e6af5fcc7c8f4c900ed9a6e04686841d
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-11-03 18:55:11 +0100

    Refactor

--- src/Coal/Compiler/Module/Bundle.hs
+++ src/Coal/Compiler/Module/Bundle.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Coal.Compiler.Module.Bundle where
 
+import Coal.Ast.Metadata (Metadata (..))
 import Coal.Common.Environment (Environment (..))
+import Coal.Compiler.Stack
 import Coal.Language
+import Coal.Language.Module
 import Data.Map.Strict (Map)
-import Extras (Dictionary, Name, Set)
+import Extras (Dictionary, Name, Set, forM_)
 
 type IndexedConstructor = DataConstructor TypeIndex Kind IndexedType
 
@@ -50,11 +55,36 @@ data ModuleBundle = ModuleBundle
   , moduleAliases :: Environment AliasInfo
   , moduleNames :: Environment NameInfo
   , moduleExports :: Set Name
---  , moduleDefinitions :: 
---  , moduleObjectCode :: ByteString
+  --  , moduleDefinitions ::
+  --  , moduleObjectCode :: ByteString
   }
   deriving (Show, Eq, Ord, Read)
 
-build :: a -> ModuleBundle
-build =
+build :: (Monad m) => Module Metadata Kind () -> CompilerT Metadata m ModuleBundle
+build (Module _ _ defs) = do
+  forM_ defs build2
   undefined
+
+build2 :: (Monad m) => Definition Metadata Kind () -> CompilerT Metadata m ModuleBundle
+build2 =
+  \case
+    DType loc name d ->
+      undefined
+    DCotype loc name d ->
+      undefined
+    DFunction loc name fs ds ->
+      undefined
+    DConstant loc name d ds ->
+      undefined
+    DImport loc path ns ->
+      undefined
+    DTrait loc name t ->
+      undefined
+    DInstance loc name d ->
+      undefined
+    DTypeAlias loc name a ->
+      undefined
+    DFold loc name d ->
+      undefined
+    DUnfold loc name d ->
+      undefined
