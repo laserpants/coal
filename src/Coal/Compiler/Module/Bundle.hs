5a3ddcef5a9baa4c0eabb9965362cd4666f63f42
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-11-03 15:56:37 +0100

    Refactor

---
+++ src/Coal/Compiler/Module/Bundle.hs
@@ -0,0 +1,34 @@
+module Coal.Compiler.Module.Bundle where
+
+import Coal.Common.Environment (Environment (..))
+import Coal.Language
+import Data.Map.Strict (Map)
+import Extras (Dictionary, Name, Set)
+
+type IndexedConstructor = DataConstructor TypeIndex Kind IndexedType
+
+data DataConstructorInfo
+  = DataConstructorInfo IndexedConstructor (Set Name)
+  deriving (Show, Eq, Ord, Read)
+
+data TraitInfo
+  = TraitInfo
+      (Parameter Kind)
+      (TypeIndex Kind)
+      (Environment IndexedScheme)
+  deriving (Show, Eq, Ord, Read)
+
+data InstanceInfo
+  = InstanceInfo
+      (Map IndexedType ParameterizedType)
+      (Dictionary IndexedScheme)
+  deriving (Show, Eq, Ord, Read)
+
+data ModuleBundle = ModuleBundle
+  { moduleDataConstructors :: Environment DataConstructorInfo
+  , moduleTypeConstructors :: Environment Kind
+  , moduleTraits :: Environment TraitInfo
+  , moduleInstances :: Environment InstanceInfo
+  , moduleAliases :: Int
+  }
+  deriving (Show, Eq, Ord, Read)
