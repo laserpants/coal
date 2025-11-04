7831026125767d562752807547cb7a1fb879a126
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-11-04 08:48:10 +0100

    Refactor

--- src/Coal/Compiler/Module/Bundle.hs
+++ src/Coal/Compiler/Module/Bundle.hs
@@ -1,36 +1,43 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Coal.Compiler.Module.Bundle where
 
 import Coal.Ast.Metadata (Metadata (..))
 import Coal.Common.Environment (Environment (..))
+import qualified Coal.Common.Environment as Environment
 import Coal.Compiler.Stack
 import Coal.Language
 import Coal.Language.Module
+import Control.Monad.State (StateT, modify, execStateT)
 import Data.Map.Strict (Map)
+import qualified Data.Set as Set
 import Extras (Dictionary, Name, Set, forM_)
 
 type IndexedConstructor = DataConstructor TypeIndex Kind IndexedType
 
 data DataConstructorInfo
-  = DataConstructorInfo IndexedConstructor (Set Name)
+  = DataConstructorInfo Metadata Name IndexedConstructor (Set Name)
+  deriving (Show, Eq, Ord, Read)
+
+data CodataAccessorInfo
+  = CodataAccessorInfo Metadata Name (CodataAccessor TypeIndex Kind IndexedType)
+  deriving (Show, Eq, Ord, Read)
+
+data TypeConstructorInfo
+  = TypeConstructorInfo Metadata Name Kind
   deriving (Show, Eq, Ord, Read)
 
 data TraitInfo
-  = TraitInfo
-      (Parameter Kind)
-      (TypeIndex Kind)
-      (Environment IndexedScheme)
+  = TraitInfo Metadata Name (Parameter Kind) (TypeIndex Kind) (Environment IndexedScheme)
   deriving (Show, Eq, Ord, Read)
 
 data InstanceInfo
-  = InstanceInfo
-      (Map IndexedType ParameterizedType)
-      (Dictionary IndexedScheme)
+  = InstanceInfo Metadata Name (Map IndexedType ParameterizedType) (Dictionary IndexedScheme)
   deriving (Show, Eq, Ord, Read)
 
 data AliasInfo
-  = AliasInfo [Name] ParameterizedType
+  = AliasInfo Metadata Name [Name] ParameterizedType
   deriving (Show, Eq, Ord, Read)
 
 data NameInfo
@@ -44,12 +51,13 @@ data NameInfo
   | IType
   | ICotype
   | ITrait
+  | IAlias
   deriving (Show, Eq, Ord, Read)
 
 data ModuleBundle = ModuleBundle
   { moduleDataConstructors :: Environment DataConstructorInfo
-  , moduleCodataAccessors :: Environment (CodataAccessor TypeIndex Kind IndexedType)
-  , moduleTypeConstructors :: Environment Kind
+  , moduleCodataAccessors :: Environment CodataAccessorInfo
+  , moduleTypeConstructors :: Environment TypeConstructorInfo
   , moduleTraits :: Environment TraitInfo
   , moduleInstances :: Environment InstanceInfo
   , moduleAliases :: Environment AliasInfo
@@ -60,31 +68,77 @@ data ModuleBundle = ModuleBundle
   }
   deriving (Show, Eq, Ord, Read)
 
-build :: (Monad m) => Module Metadata Kind () -> CompilerT Metadata m ModuleBundle
-build (Module _ _ defs) = do
-  forM_ defs build2
-  undefined
+emptyModuleBundle :: ModuleBundle
+emptyModuleBundle =
+  ModuleBundle
+    { moduleDataConstructors = mempty
+    , moduleCodataAccessors = mempty
+    , moduleTypeConstructors = mempty
+    , moduleTraits = mempty
+    , moduleInstances = mempty
+    , moduleAliases = mempty
+    , moduleNames = mempty
+    , moduleExports = mempty
+    }
 
-build2 :: (Monad m) => Definition Metadata Kind () -> CompilerT Metadata m ModuleBundle
-build2 =
+insertDataConstructor :: Name -> DataConstructorInfo -> ModuleBundle -> ModuleBundle
+insertDataConstructor name info ModuleBundle{..} = ModuleBundle{moduleDataConstructors = Environment.insert name info moduleDataConstructors, ..}
+
+insertCodataAccessor :: Name -> CodataAccessorInfo -> ModuleBundle -> ModuleBundle
+insertCodataAccessor name info ModuleBundle{..} = ModuleBundle{moduleCodataAccessors = Environment.insert name info moduleCodataAccessors, ..}
+
+insertTypeConstructor :: Name -> TypeConstructorInfo -> ModuleBundle -> ModuleBundle
+insertTypeConstructor name info ModuleBundle{..} = ModuleBundle{moduleTypeConstructors = Environment.insert name info moduleTypeConstructors, ..}
+
+insertTrait :: Name -> TraitInfo -> ModuleBundle -> ModuleBundle
+insertTrait name info ModuleBundle{..} = ModuleBundle{moduleTraits = Environment.insert name info moduleTraits, ..}
+
+insertInstance :: Name -> InstanceInfo -> ModuleBundle -> ModuleBundle
+insertInstance name info ModuleBundle{..} = ModuleBundle{moduleInstances = Environment.insert name info moduleInstances, ..}
+
+insertAlias :: Name -> AliasInfo -> ModuleBundle -> ModuleBundle
+insertAlias name info ModuleBundle{..} = ModuleBundle{moduleAliases = Environment.insert name info moduleAliases, ..}
+
+addName :: Name -> NameInfo -> ModuleBundle -> ModuleBundle
+addName name info ModuleBundle{..} = ModuleBundle{moduleNames = Environment.insert name info moduleNames, ..}
+
+addExport :: Name -> ModuleBundle -> ModuleBundle
+addExport name ModuleBundle{..} = ModuleBundle{moduleExports = Set.insert name moduleExports, ..}
+
+setExports :: [Name] -> ModuleBundle -> ModuleBundle
+setExports names ModuleBundle{..} = ModuleBundle{moduleExports = Set.fromList names}  
+
+typeConstructorInfo :: Metadata -> Name -> TypeDef -> TypeConstructorInfo
+typeConstructorInfo loc name (TypeDef ps _) = TypeConstructorInfo loc name kind
+ where
+  kind = foldr KArrow KType (replicate (length ps) KType)
+
+build :: (Monad m) => Module Metadata Kind () -> CompilerT Metadata m ModuleBundle
+build (Module _ exports defs) =
+  flip execStateT emptyModuleBundle $ do
+    modify (setExports exports)
+    forM_ defs buildDefinition
+
+buildDefinition :: (Monad m) => Definition Metadata Kind () -> StateT ModuleBundle (CompilerT Metadata m) ()
+buildDefinition =
   \case
-    DType loc name d ->
+    DType loc name def -> do
+      modify (insertTypeConstructor name (typeConstructorInfo loc name def))
+    DCotype loc name d -> do
+      modify (insertCodataAccessor name undefined)
+    DFunction loc name fs ds -> do
       undefined
-    DCotype loc name d ->
+    DConstant loc name d ds -> do
       undefined
-    DFunction loc name fs ds ->
+    DTrait loc name t -> do
       undefined
-    DConstant loc name d ds ->
+    DInstance loc name d -> do
       undefined
-    DImport loc path ns ->
+    DTypeAlias loc name a -> do
       undefined
-    DTrait loc name t ->
+    DFold loc name d -> do
       undefined
-    DInstance loc name d ->
+    DUnfold loc name d -> do
       undefined
-    DTypeAlias loc name a ->
-      undefined
-    DFold loc name d ->
-      undefined
-    DUnfold loc name d ->
-      undefined
+    DImport loc path ns -> do
+      pure ()
