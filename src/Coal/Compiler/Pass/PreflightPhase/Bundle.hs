803e15e207d3d58743573c34cdf5a0e0a7a89d8f
Johannes Heikki Hilden <johannes@greatgraphics.se>
2025-11-12 19:21:28 +0100

    wip

---
+++ src/Coal/Compiler/Pass/PreflightPhase/Bundle.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Coal.Compiler.Pass.PreflightPhase.Bundle (passBundle) where
+
+import Coal.Ast.Metadata (Metadata (..))
+import Coal.Compiler.Module.Builders
+import Coal.Compiler.Pass (Pass (..))
+import Coal.Compiler.Stack
+import Coal.Language
+import Coal.Language.Module
+import Control.Monad.State
+
+passBundle :: (Monad m) => Pass Metadata m [Module Metadata Kind ()] [Module Metadata Kind ()]
+passBundle =
+  Pass
+    { passName = "Bundle"
+    , runPass = pass
+    }
+
+pass :: (Monad m) => [Module Metadata Kind ()] -> CompilerT Metadata m [Module Metadata Kind ()]
+pass modules = do
+  forM_ modules $
+    \m@(Module p _ _) -> do
+      b <- build m
+      insertModuleC (principalPath p) b
+  pure modules
