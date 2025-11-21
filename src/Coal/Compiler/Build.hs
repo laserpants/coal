30ff300761e3d90e95188065bda6dcc0f9c3e3f0
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-11-21 10:14:49 +0300

    wip

--- src/Coal/Compiler/Build.hs
+++ src/Coal/Compiler/Build.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 
@@ -51,6 +50,7 @@ module Coal.Compiler.Build (
 import Coal.AST.Type.Parameterized (instantiateVars)
 import Coal.Common.Environment (Environment (..))
 import qualified Coal.Common.Environment as Environment
+import Coal.Compiler.Build.NameInfo
 import Coal.Language
 import Coal.Language.Module (AliasDef (..), CotypeDef (..), Path (Path), TraitDef (..), TypeDef (..))
 import Control.Monad.State (evalState)
@@ -58,136 +58,7 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as Set
-import Extras (Dictionary, Name, Set, for)
-
-type IndexedConstructor = DataConstructor TypeIndex Kind IndexedType
-
-data DataConstructorInfo a = DataConstructorInfo
-  { dataConstructorInfoMetaData :: a
-  , dataConstructorInfoName :: Name
-  , dataConstructorInfoContructor :: IndexedConstructor
-  , dataConstructorInfoNameSet :: Set Name
-  }
-  deriving (Show, Eq, Ord, Read)
-
-type IndexedCodataAccessor = CodataAccessor TypeIndex Kind IndexedType
-
-data CodataAccessorInfo a = CodataAccessorInfo
-  { codataAccessorInfoMetadata :: a
-  , codataAccessorInfoName :: Name
-  , codataAccessorInfoAccessor :: IndexedCodataAccessor
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data TypeConstructorInfo a = TypeConstructorInfo
-  { typeConstructorInfoMetadata :: a
-  , typeConstructorInfoName :: Name
-  , typeConstructorInfoKind :: Kind
-  , typeConstructorInfoDataConstructors :: [Name]
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data CotypeConstructorInfo a = CotypeConstructorInfo
-  { cotypeConstructorInfoMetadata :: a
-  , cotypeConstructorInfoName :: Name
-  , cotypeConstructorInfoKind :: Kind
-  , cotypeConstructorInfoDataAccessors :: [Name]
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data TraitInfo a = TraitInfo
-  { traitInfoMetadata :: a
-  , traitInfoName :: Name
-  , traitInfoParameter :: Parameter Kind
-  , traitInfoEntries :: Environment (Scheme Parameter () ParameterizedType)
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data InstanceInfo a = InstanceInfo
-  { instanceInfoMetadata :: a
-  , instanceInfoType :: ParameterizedType
-  , instanceInfoIndexedType :: IndexedType
-  , instanceInfoEntries :: Dictionary IndexedScheme
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data AliasInfo a = AliasInfo
-  { aliasInfoMetadata :: a
-  , aliasInfoName :: Name
-  , aliasInfoParams :: [Name]
-  , aliasInfoType :: ParameterizedType
-  }
-  deriving (Show, Eq, Ord, Read)
-
-data NameInfo
-  = IFunction Name IndexedScheme
-  | IConstant Name IndexedScheme
-  | IFold Name IndexedScheme
-  | IUnfold Name IndexedScheme
-  | IDataConstructor Name IndexedScheme
-  | ICodataAccessor Name IndexedScheme
-  | IType Name Kind
-  | ICotype Name Kind
-  | ITrait Name
-  | IAlias Name
-  | IFunctionPlaceholder Name
-  | IConstantPlaceholder Name
-  | IFoldPlaceholder Name
-  | IUnfoldPlaceholder Name
-  deriving (Show, Eq, Ord, Read)
-
-class HasName a where
-  nameOf :: a -> Name
-
-instance HasName NameInfo where
-  nameOf =
-    \case
-      IFunction name _ ->
-        name
-      IConstant name _ ->
-        name
-      IFold name _ ->
-        name
-      IUnfold name _ ->
-        name
-      IDataConstructor name _ ->
-        name
-      ICodataAccessor name _ ->
-        name
-      IType name _ ->
-        name
-      ICotype name _ ->
-        name
-      ITrait name ->
-        name
-      IAlias name ->
-        name
-      IFunctionPlaceholder name ->
-        name
-      IConstantPlaceholder name ->
-        name
-      IFoldPlaceholder name ->
-        name
-      IUnfoldPlaceholder name ->
-        name
-
-instance HasName (DataConstructorInfo a) where
-  nameOf = dataConstructorInfoName
-
-instance HasName (CodataAccessorInfo a) where
-  nameOf = codataAccessorInfoName
-
-instance HasName (TypeConstructorInfo a) where
-  nameOf = typeConstructorInfoName
-
-instance HasName (CotypeConstructorInfo a) where
-  nameOf = cotypeConstructorInfoName
-
-instance HasName (TraitInfo a) where
-  nameOf = traitInfoName
-
-instance HasName (Name, a) where
-  nameOf = fst
+import Extras (Name, Set, for)
 
 data ModuleBuild a = ModuleBuild
   { modulePath :: Path
