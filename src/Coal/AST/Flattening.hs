4a6361c704b2afcea1ca0727245c85ae44e62193
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-11-21 10:58:34 +0300

    wip

--- src/Coal/AST/Flattening.hs
+++ src/Coal/AST/Flattening.hs
@@ -7,8 +7,8 @@ import Coal.Language.Expression (Expression (..))
 flattenApplication :: Expression a t -> Expression a t
 flattenApplication =
   \case
-    EApplication a t (EApplication _ _ e a1) a2 ->
-      EApplication a t e (a1 <> a2)
+    EApplication a t (EApplication _ _ e e1) e2 ->
+      EApplication a t e (e1 <> e2)
     expr ->
       expr
 
