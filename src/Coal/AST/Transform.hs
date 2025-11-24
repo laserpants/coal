6fa604476c27fd0aef6256cfd8a0d62771ab66cb
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2025-11-24 19:45:58 +0300

    Naming changes

--- src/Coal/AST/Transform.hs
+++ src/Coal/AST/Transform.hs
@@ -11,56 +11,56 @@ import Control.Monad.Identity (runIdentity)
 import Data.Data (Data)
 import Extras (Name, const2, (<$$>))
 
-class TransformContext e where
-  transform :: (Monad m, Data a, Data t, Ord t) => Name -> (a -> t -> m (Expression a t)) -> e a t -> m (e a t)
+class Transformable e where
+  rewrite :: (Monad m, Data a, Data t, Ord t) => Name -> (a -> t -> m (Expression a t)) -> e a t -> m (e a t)
 
-instance TransformContext (Binding Expression) where
-  transform name f =
+instance Transformable (Binding Expression) where
+  rewrite name f =
     \case
       BPattern a p e ->
-        BPattern a p <$> transform name f e
+        BPattern a p <$> rewrite name f e
       BFunction a n ps e ->
-        BFunction a name ps <$> transform n f e
+        BFunction a name ps <$> rewrite n f e
 
-instance TransformContext (Guard Expression) where
-  transform name f =
+instance Transformable (Guard Expression) where
+  rewrite name f =
     \case
       CGuard e ->
-        CGuard <$> transform name f e
+        CGuard <$> rewrite name f e
 
-instance TransformContext (Choice Expression) where
-  transform name f =
+instance Transformable (Choice Expression) where
+  rewrite name f =
     \case
       CPlain a gs e ->
         CPlain a
-          <$> traverse (transform name f) gs
-          <*> transform name f e
+          <$> traverse (rewrite name f) gs
+          <*> rewrite name f e
       CLambda{} ->
         error "TODO"
 
-instance TransformContext Clause where
-  transform name f =
+instance Transformable Clause where
+  rewrite name f =
     \case
       EClause a ps cs
         | name `isNotBoundIn` ps ->
-            EClause a ps <$> traverse (transform name f) cs
+            EClause a ps <$> traverse (rewrite name f) cs
         | otherwise ->
             pure (EClause a ps cs)
 
-instance TransformContext CompiledClause where
-  transform name f =
+instance Transformable CompiledClause where
+  rewrite name f =
     \case
       ECompiledClause loc lls e
         | name `isNotBoundIn` lls ->
-            ECompiledClause loc lls <$> transform name f e
+            ECompiledClause loc lls <$> rewrite name f e
         | otherwise ->
             pure (ECompiledClause loc lls e)
 
-instance TransformContext Expression where
-  transform name f =
+instance Transformable Expression where
+  rewrite name f =
     \case
       EAnnotation _ _ e ->
-        transform name f e
+        rewrite name f e
       var@(EVariable a (Label t name1))
         | name == name1 ->
             f a t
@@ -68,36 +68,36 @@ instance TransformContext Expression where
             pure var
       expr@(ELambda a ps e)
         | name `isNotBoundIn` ps ->
-            ELambda a ps <$> transform name f e
+            ELambda a ps <$> rewrite name f e
         | otherwise ->
             pure expr
       ELet a gs e1 ->
         ELet a
-          <$> traverse (transform name f) gs
+          <$> traverse (rewrite name f) gs
           <*> ( if name `isNotBoundIn` gs
-                  then transform name f e1
+                  then rewrite name f e1
                   else pure e1
               )
       expr@(ERecursiveLet a p e1 e2) ->
         if name `isNotBoundIn` p
           then
             ERecursiveLet a p
-              <$> transform name f e1
-              <*> transform name f e2
+              <$> rewrite name f e1
+              <*> rewrite name f e2
           else pure expr
       ERecord a t d e ->
         ERecord a t
-          <$> traverse (transform name f) d
-          <*> traverse (transform name f) e
+          <$> traverse (rewrite name f) d
+          <*> traverse (rewrite name f) e
       ESelect a ll e ->
-        ESelect a ll <$> transform name f e
+        ESelect a ll <$> rewrite name f e
       EFocus field ll1 ll2 e1 e2 -> do
-        EFocus field ll1 ll2 <$> transform name f e1 <*> transform name f e2
+        EFocus field ll1 ll2 <$> rewrite name f e1 <*> rewrite name f e2
       EIf a t e1 e2 e3 ->
         EIf a t
-          <$> transform name f e1
-          <*> transform name f e2
-          <*> transform name f e3
+          <$> rewrite name f e1
+          <*> rewrite name f e2
+          <*> rewrite name f e3
       expr@ELiteral{} ->
         pure expr
       EConstructor a ll@(Label t name1)
@@ -107,47 +107,47 @@ instance TransformContext Expression where
             pure (EConstructor a ll)
       EApplication a t e1 es ->
         EApplication a t
-          <$> transform name f e1
-          <*> traverse (transform name f) es
+          <$> rewrite name f e1
+          <*> traverse (rewrite name f) es
       EListCons a t e1 e2 ->
         EListCons a t
-          <$> transform name f e1
-          <*> transform name f e2
+          <$> rewrite name f e1
+          <*> rewrite name f e2
       EMatch a t e cs ->
         EMatch a t
-          <$> transform name f e
-          <*> traverse (transform name f) cs
+          <$> rewrite name f e
+          <*> traverse (rewrite name f) cs
       ELambdaMatch a t cs me ->
         ELambdaMatch a t
-          <$> traverse (transform name f) cs
-          <*> traverse (transform name f) me
+          <$> traverse (rewrite name f) cs
+          <*> traverse (rewrite name f) me
       ECompiledMatch a t e cs ->
         ECompiledMatch a t
-          <$> transform name f e
-          <*> traverse (transform name f) cs
+          <$> rewrite name f e
+          <*> traverse (rewrite name f) cs
       EFold a t es cs me ->
         EFold a t
-          <$> traverse (transform name f) es
-          <*> traverse (transform name f) cs
-          <*> traverse (transform name f) me
+          <$> traverse (rewrite name f) es
+          <*> traverse (rewrite name f) cs
+          <*> traverse (rewrite name f) me
       ECodataSelect a ll e1 e2 ->
         ECodataSelect a ll
-          <$> traverse (transform name f) e1
-          <*> traverse (transform name f) e2
+          <$> traverse (rewrite name f) e1
+          <*> traverse (rewrite name f) e2
       expr@EUnaryOperator{} ->
         pure expr
       expr@EBinaryOperator{} ->
         pure expr
       EListLiteral a t es ->
-        EListLiteral a t <$> traverse (transform name f) es
+        EListLiteral a t <$> traverse (rewrite name f) es
       ETuple a t es ->
-        ETuple a t <$> traverse (transform name f) es
+        ETuple a t <$> traverse (rewrite name f) es
       ECodataRecord a t d ->
-        ECodataRecord a t <$> traverse (transform name f) d
+        ECodataRecord a t <$> traverse (rewrite name f) d
       EFFICall a ll es e ->
         EFFICall a ll
-          <$> traverse (transform name f) es
-          <*> transform name f e
+          <$> traverse (rewrite name f) es
+          <*> rewrite name f e
       _ ->
         error "TODO"
 
@@ -156,7 +156,7 @@ isNotBoundIn :: (BoundVars b) => Name -> b -> Bool
 isNotBoundIn name obj = name `notElem` boundIn obj
 
 replace :: (Ord t, Data a, Data t) => Name -> (a -> t -> Expression a t) -> Expression a t -> Expression a t
-replace name f = runIdentity . transform name (pure <$$> f)
+replace name f = runIdentity . rewrite name (pure <$$> f)
 
 replaceWith :: (Ord t, Data a, Data t) => Name -> Expression a t -> Expression a t -> Expression a t
 replaceWith name = replace name . const2
