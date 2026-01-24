9caa72d8736e1055867231afb1d7dbc80f01a93c
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-01-24 18:22:27 +0300

    wip

--- test/Coal/ProtoCompiler/ProtoBuildSpec.hs
+++ test/Coal/ProtoCompiler/ProtoBuildSpec.hs
@@ -1,21 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Coal.ProtoCompiler.ProtoBuildSpec where
 
-import Coal.ProtoLanguage.ProtoModule (ProtoModule (..)) 
+import Coal.Language.Module.Path (Path (..))
+import Coal.ProtoLanguage.ProtoDefinition (ProtoDefinition (..))
+import Coal.ProtoLanguage.ProtoModule (ProtoModule (..))
 
+testModule1 :: (Monoid a) => ProtoModule a
 testModule1 =
   ProtoModule
-    { protoOmodulePath = undefined
-    , protoOmoduleDefinitions = []
+    { protoOmodulePath = Path ["Main"]
+    , protoOmoduleDefinitions =
+        [ ProtoDFunction
+            mempty
+            "main"
+        ]
     }
 
+testModule2 :: (Monoid a) => ProtoModule a
 testModule2 =
   ProtoModule
-    { protoOmodulePath = undefined
-    , protoOmoduleDefinitions = []
+    { protoOmodulePath = Path ["Math"]
+    , protoOmoduleDefinitions =
+        [ ProtoDFunction
+            mempty
+            "factorial"
+        ]
     }
 
+testModule3 :: (Monoid a) => ProtoModule a
 testModule3 =
   ProtoModule
-    { protoOmodulePath = undefined
-    , protoOmoduleDefinitions = []
+    { protoOmodulePath = Path ["Nat"]
+    , protoOmoduleDefinitions =
+        [ ProtoDFunction
+            mempty
+            "pack"
+        , ProtoDFunction
+            mempty
+            "unpack"
+        ]
     }
