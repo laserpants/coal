624ac58a60bef887635cd85fcccde54700c8f89a
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-01-24 20:27:32 +0300

    wip

--- src/Coal/ProtoCompiler/ProtoBuild/ProtoPrep.hs
+++ src/Coal/ProtoCompiler/ProtoBuild/ProtoPrep.hs
@@ -1,3 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+
 module Coal.ProtoCompiler.ProtoBuild.ProtoPrep where
 
-protoOprepareBuild = undefined
+import Coal.ProtoLanguage.ProtoModule (ProtoModule (..))
+import Coal.ProtoCompiler.ProtoStack (ProtoCompilerT (..))
+import Coal.ProtoCompiler.ProtoBuild (ProtoBuild (..))
+
+protoOprepareBuild :: ProtoModule a t -> ProtoCompilerT m ProtoBuild
+protoOprepareBuild ProtoModule{..} = do
+  undefined
