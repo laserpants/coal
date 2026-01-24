de4f50ed701a710eaf15e48d9ac9cb7dd0b09e1b
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-01-24 16:04:16 +0300

    wip

--- src/Coal/ProtoLanguage/ProtoDefinition.hs
+++ src/Coal/ProtoLanguage/ProtoDefinition.hs
@@ -8,24 +8,33 @@ module Coal.ProtoLanguage.ProtoDefinition where
 
 import Data.Data (Data, Typeable)
 
-data ProtoDefinition 
-  = ProtoDType
-  | ProtoDTypeAlias
-  | ProtoDFunction
-  | ProtoDFold
-  | ProtoDLet
-  | ProtoDImport
-  | ProtoDQualifiedImport
-  | ProtoDTrait
-  | ProtoDInstance
-  deriving 
-    (Show, 
-    Eq, 
-    Ord, 
-    Read, 
---    Functor, 
---    Foldable, 
---    Traversable, 
-    Data, 
-    Typeable
+data ProtoDefinition
+  = -- | Type definition
+    ProtoDType
+  | -- | Type alias
+    ProtoDTypeAlias
+  | -- | Function definition
+    ProtoDFunction
+  | -- | Top-level fold
+    ProtoDFold
+  | -- | Top-level let-binding
+    ProtoDLet
+  | -- | Import statement
+    ProtoDImport
+  | -- | Namespace (qualified) import
+    ProtoDQualifiedImport
+  | -- | Trait
+    ProtoDTrait
+  | -- | Trait instance
+    ProtoDInstance
+  deriving
+    ( Show
+    , Eq
+    , Ord
+    , Read
+    , --    Functor,
+      --    Foldable,
+      --    Traversable,
+      Data
+    , Typeable
     )
