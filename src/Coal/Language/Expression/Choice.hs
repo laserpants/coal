47ef68ed67bfb1dba0162c7b43eb7684daeada42
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-01-30 22:58:57 +0300

    wip

--- src/Coal/Language/Expression/Choice.hs
+++ src/Coal/Language/Expression/Choice.hs
@@ -28,10 +28,15 @@ newtype Guard e a s t = CGuard {guardExpression :: e a s t}
     , Generic
     )
 
-instance (Ord t, Data a, Data s, Data t, Data (e a s t), Typeable e) => FreeVars (Guard e a s t) t where
+instance
+  (Ord t, Data a, Data s, Data t, Data (e a s t), Typeable e) =>
+  FreeVars (Guard e a s t) t
+  where
   freeIn = Set.fromList . universeBi
 
-instance (Binary (e a s t), Binary a, Binary s, Binary t) => Binary (Guard e a s t)
+instance
+  (Binary (e a s t), Binary a, Binary s, Binary t) =>
+  Binary (Guard e a s t)
 
 data Choice e a s t = CPlain a [Guard e a s t] (e a s t)
   deriving
@@ -47,7 +52,12 @@ data Choice e a s t = CPlain a [Guard e a s t] (e a s t)
     , Generic
     )
 
-instance (Ord t, Data a, Data s, Data t, Data (e a s t), Typeable e) => FreeVars (Choice e a s t) t where
+instance
+  (Ord t, Data a, Data s, Data t, Data (e a s t), Typeable e) =>
+  FreeVars (Choice e a s t) t
+  where
   freeIn = Set.fromList . universeBi
 
-instance (Binary (e a s t), Binary a, Binary s, Binary t) => Binary (Choice e a s t)
+instance
+  (Binary (e a s t), Binary a, Binary s, Binary t) =>
+  Binary (Choice e a s t)
