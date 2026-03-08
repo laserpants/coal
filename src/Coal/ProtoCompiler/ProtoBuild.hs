833efa78d76726c7c136bb0c75a9ec5434eb6f7e
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-03-08 14:20:08 +0100

    wip

--- src/Coal/ProtoCompiler/ProtoBuild.hs
+++ src/Coal/ProtoCompiler/ProtoBuild.hs
@@ -39,6 +39,7 @@ import Coal.ProtoCompiler.ProtoBuild.ProtoNameEntry
 import Coal.ProtoLanguage.ProtoDefinition (ProtoDefinition (..))
 import Control.Monad.State (execState, modify)
 import Data.Binary
+import Data.List (nub)
 import Data.ByteString (ByteString)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -114,7 +115,7 @@ overBuildNames f ProtoBuild{..} =
 
 insertBuildNameEntry :: ProtoNameEntry -> ProtoBuild a -> ProtoBuild a
 insertBuildNameEntry entry =
-  overBuildNames (Environment.insertWith (<>) name [entry])
+  overBuildNames (Environment.adjust nub name . Environment.insertWith (<>) name [entry])
  where
   name = protoOnameOf entry
 
