0443cdf17c421d0f6c95ee390ca490bf9cd91ff2
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-03-20 11:53:21 +0100

    wip

--- src/Coal/Compiler/Pass/TypePhase/ExpandAliases.hs
+++ src/Coal/Compiler/Pass/TypePhase/ExpandAliases.hs
@@ -6,7 +6,8 @@ import Coal.Compiler.Aliases
 import Coal.Compiler.Pass (Pass (..))
 import Coal.Language.Type
 import Coal.ProtoLanguage.ProtoModule (ProtoModule (..))
+import Control.Monad.IO.Class (MonadIO)
 import Data.Data (Data)
 
-passExpandAliases :: (Monad m, Data a, Data k, AliasTransform (Type Parameter k)) => Pass a m (ProtoModule a k ()) (ProtoModule a k ())
+passExpandAliases :: (MonadIO m, Data a, Data k, AliasTransform (Type Parameter k)) => Pass a m (ProtoModule a k ()) (ProtoModule a k ())
 passExpandAliases = Pass{runPass = aliasTransform}
