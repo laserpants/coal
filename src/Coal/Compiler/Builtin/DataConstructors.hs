789a2bf20b9c203e08827b8cdbabebc7e6e356dd
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-11 09:56:34 +0200

    wip

--- src/Coal/Compiler/Builtin/DataConstructors.hs
+++ src/Coal/Compiler/Builtin/DataConstructors.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Coal.Compiler.Builtin.DataConstructors (builtinDataConstructors2) where
+module Coal.Compiler.Builtin.DataConstructors (builtinDataConstructors) where
 
 import Coal.Language
 import Coal.ProtoCompiler.ProtoBuild.ProtoNameEntry
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.Set as Set
 
-builtinDataConstructors2 :: (Monoid a) => [ProtoDataConstructorEntry a]
-builtinDataConstructors2 =
+builtinDataConstructors :: (Monoid a) => [ProtoDataConstructorEntry a]
+builtinDataConstructors =
   [ ProtoDataConstructorEntry
       { protoOdataConstructorEntryMetaData = mempty
       , protoOdataConstructorEntryName = "Zero"
@@ -89,69 +89,3 @@ builtinDataConstructors2 =
           Set.fromList ["Process"]
       }
   ]
-
--- builtinDataConstructors :: (Monoid a) => [(Name, DataConstructorEntry a)]
--- builtinDataConstructors =
---  [
---    ( "Zero"
---    , DataConstructorEntry
---        mempty
---        "Zero"
---        ( DataConstructor
---            "Zero"
---            0
---            (Forall mempty mempty (TIntrinsic INat))
---        )
---        (Set.fromList ["Succ", "Zero"])
---    )
---  ,
---    ( "Succ"
---    , DataConstructorEntry
---        mempty
---        "Succ"
---        ( DataConstructor
---            "Succ"
---            1
---            (Forall mempty mempty (TIntrinsic INat `TArrow` TIntrinsic INat))
---        )
---        (Set.fromList ["Succ", "Zero"])
---    )
---  ,
---    ( "Process"
---    , DataConstructorEntry
---        mempty
---        "Process"
---        ( DataConstructor
---            "Process"
---            1
---            ( Forall
---                (Set.fromList [TypeIndex KType 0, TypeIndex KType 1])
---                mempty
---                ( TRecord
---                    ( TRow
---                        ( RExtend
---                            "state"
---                            (TVariable (TypeIndex KType 0))
---                            ( RExtend
---                                "step"
---                                ( TVariable (TypeIndex KType 1)
---                                    `TArrow` TVariable (TypeIndex KType 0)
---                                    `TArrow` applyTypeArgs
---                                      KType
---                                      (TConstructor (KArrow KType (KArrow KType KType)) "Process")
---                                      (TVariable (TypeIndex KType 0) :| [TVariable (TypeIndex KType 1)])
---                                )
---                                RNil
---                            )
---                        )
---                    )
---                    `TArrow` applyTypeArgs
---                      KType
---                      (TConstructor (KArrow KType (KArrow KType KType)) "Process")
---                      (TVariable (TypeIndex KType 0) :| [TVariable (TypeIndex KType 1)])
---                )
---            )
---        )
---        (Set.fromList ["Process"])
---    )
---  ]
--- src/Coal/ProtoCompiler/ProtoError.hs
+++
@@ -1,5 +0,0 @@
-module Coal.ProtoCompiler.ProtoError (ProtoError (..)) where
-
--- TODO
-data ProtoError = ProtoError
-  deriving (Show, Eq)
