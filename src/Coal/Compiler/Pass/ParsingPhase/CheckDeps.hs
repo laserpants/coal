ec07cec148c6b48385ee5f7bc700fe5c9cdb24a7
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-13 20:19:29 +0200

    wip

--- src/Coal/Compiler/Pass/ParsingPhase/CheckDeps.hs
+++ src/Coal/Compiler/Pass/ParsingPhase/CheckDeps.hs
@@ -12,22 +12,21 @@ import Coal.Compiler.Journal (tellErrors)
 import Coal.Compiler.Pass (Pass (..))
 import Coal.Compiler.Stack
 import Coal.Compiler.State
-import Coal.Language.Module (Module (..), ModuleExportList (..))
+import Coal.Language.Module (Module (..))
 import Coal.Language.Module.Path (principalPath)
 import Coal.Parser (parseModule)
 import Coal.Parser.Core (spaces)
 import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.State (get)
-import Data.Text (Text)
 import Extras (Name)
 import Text.Megaparsec (eof, runParser)
 
 passCheckDeps :: (MonadIO m) => Pass Metadata m [BuildEnvelope (Module Metadata () ())] [BuildEnvelope (Module Metadata () ())]
-passCheckDeps = Pass{runPass = pass}
+passCheckDeps = Pass{runPass = passImpl}
 
-pass :: (MonadIO m) => [BuildEnvelope (Module Metadata () ())] -> CompilerT Metadata m [BuildEnvelope (Module Metadata () ())]
-pass = traverse check
+passImpl :: (MonadIO m) => [BuildEnvelope (Module Metadata () ())] -> CompilerT Metadata m [BuildEnvelope (Module Metadata () ())]
+passImpl = traverse check
 
 check :: (MonadIO m) => BuildEnvelope (Module Metadata () ()) -> CompilerT Metadata m (BuildEnvelope (Module Metadata () ()))
 check =
@@ -38,9 +37,7 @@ check =
       CompilerState{..} <- get
       if any (\dep -> principalPath dep `elem` compilerToBeRecompiled) buildDependencies
         then do
-          let name = principalPath buildPath
-          src <- getSourceC name
-          res <- fromSource name src
+          res <- buildFromSource (principalPath buildPath)
           case res of
             Left e -> do
               tellErrors [e]
@@ -49,8 +46,9 @@ check =
               pure r
         else pure (BCached Build{..})
 
-fromSource :: (MonadIO m) => Name -> Text -> CompilerT Metadata m (Either (CompilerError Metadata) (BuildEnvelope (Module Metadata () ())))
-fromSource name src = do
+buildFromSource :: (MonadIO m) => Name -> CompilerT Metadata m (Either (CompilerError Metadata) (BuildEnvelope (Module Metadata () ())))
+buildFromSource name = do
+  src <- getSourceC name
   toBeRecompiled name
   case runParser (spaces *> parseModule <* eof) "" src of
     Left{} ->
