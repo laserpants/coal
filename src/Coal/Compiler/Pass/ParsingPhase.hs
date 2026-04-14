a6597a60eb1ece166f0bbb50dcd8bb46831fab91
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-14 14:05:14 +0200

    wip

--- src/Coal/Compiler/Pass/ParsingPhase.hs
+++ src/Coal/Compiler/Pass/ParsingPhase.hs
@@ -9,5 +9,3 @@ import Control.Monad.IO.Class (MonadIO)
 
 parsingPhase :: (MonadIO m) => Pass Metadata m [FilePath] [BuildEnvelope (Module Metadata () ())]
 parsingPhase = passParsing
-
---    >-> mapPass (liftPass (generateDebugArtifacts "Parsing"))
