6f30a93c938c066c90ea8887ebbdd0b8f9d04e22
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-14 09:38:43 +0200

    wip

--- src/Coal/Compiler/Pass/TypePhase/Prep2.hs
+++ src/Coal/Compiler/Pass/TypePhase/Prep2.hs
@@ -6,42 +6,12 @@ import Coal.Compiler.Pass (Pass (..))
 import Coal.Compiler.Stack
 import Coal.Language (Kind)
 import Coal.Language.Module (Module (..))
-import Coal.Language.Type.Kind.Indexed (ToKindIndexed (..))
 import Control.Monad.Except (MonadIO)
-import Control.Monad.Trans (lift)
 
 passPrep2 :: (MonadIO m) => Pass Metadata m (Module Metadata Kind ()) (Module Metadata Kind ())
-passPrep2 = Pass{runPass = pass}
+passPrep2 = Pass{runPass = passImpl}
 
-pass :: (MonadIO m) => Module Metadata Kind () -> CompilerT Metadata m (Module Metadata Kind ())
-pass m = do
-  --  setCompilerCurrentModuleC (modulePath m)
-  --  lift $ setCurrentPathC (modulePath m)
-  prep m
-
--- withCurrentModuleC prep
-
-prep :: (MonadIO m) => Module Metadata Kind () -> CompilerT Metadata m (Module Metadata Kind ())
-prep m = do
-  --  m1 <- lift $ do
-  --    let modul = toModule [] m
-  --    clearAssumptionsC
-  --    clearNameStoreC
-  --    setCurrentModuleC modul
-  --    forM_ builtinFunctions $ uncurry insertNameC
-  --    toKindIndexed modul
-  --
-  --  y <- expandFunctionGroups m1
+passImpl :: (MonadIO m) => Module Metadata Kind () -> CompilerT Metadata m (Module Metadata Kind ())
+passImpl m = do
   prepareBuild m
   return m
-
---  --  clearAssumptionsC
---  --  clearNameStoreC
---  --  (next, build) <- prepareBuild m
---  --  insertCurrentModuleC build
---  --  env <- buildEnv
---  --
---  --  setNamesC env
---  --  insertNamesC builtinFunctions
---
---  pure y
