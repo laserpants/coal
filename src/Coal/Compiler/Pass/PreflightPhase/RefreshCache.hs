d3d2163aa2dcd6cf74361e18667222f1a69b6750
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-15 08:42:22 +0200

    wip

--- src/Coal/Compiler/Pass/PreflightPhase/RefreshCache.hs
+++ src/Coal/Compiler/Pass/PreflightPhase/RefreshCache.hs
@@ -3,9 +3,10 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 
-module Coal.Compiler.Pass.PreflightPhase.RefreshCache (
-  passRefreshCache,
-) where
+module Coal.Compiler.Pass.PreflightPhase.RefreshCache
+  ( passRefreshCache,
+  )
+where
 
 import Coal.AST.Metadata (Metadata (..))
 import Coal.Compiler.Build
@@ -24,7 +25,7 @@ import Extras (Name)
 import Text.Megaparsec (runParser)
 
 passRefreshCache :: (MonadIO m) => Pass Metadata m [BuildEnvelope (Module Metadata () ())] [BuildEnvelope (Module Metadata () ())]
-passRefreshCache = Pass{runPass = passImpl}
+passRefreshCache = Pass {runPass = passImpl}
 
 passImpl :: (MonadIO m) => [BuildEnvelope (Module Metadata () ())] -> CompilerT Metadata m [BuildEnvelope (Module Metadata () ())]
 passImpl = traverse refreshCache
@@ -34,25 +35,26 @@ refreshCache =
   \case
     BSource src ->
       pure (BSource src)
-    BCached Build{..} -> do
-      CompilerState{..} <- get
-      if any (\dep -> principalPath dep `elem` compilerToBeRecompiled) buildDependencies
-        then do
-          res <- compileFromSource (principalPath buildPath)
-          case res of
-            Left e -> do
-              tellErrors [e]
-              throwError PreflightFailure
-            Right r ->
-              pure r
-        else pure (BCached Build{..})
+    BCached Build {..} -> do
+      CompilerState {..} <- get
+      let touched dep = principalPath dep `elem` compilerToBeRecompiled
+       in if any touched buildDependencies
+            then do
+              res <- compileFromSource (principalPath buildPath)
+              case res of
+                Left e -> do
+                  tellErrors [e]
+                  throwError PreflightFailure
+                Right r ->
+                  pure r
+            else pure (BCached Build {..})
 
 compileFromSource :: (MonadIO m) => Name -> CompilerT Metadata m (Either (CompilerError Metadata) (BuildEnvelope (Module Metadata () ())))
 compileFromSource name = do
   src <- getSourceC name
   toBeRecompiled name
   case runParser parseSourceFile "" src of
-    Left{} ->
+    Left {} ->
       error "Implementation error"
     Right module_ -> do
       pure $ Right (BSource module_)
