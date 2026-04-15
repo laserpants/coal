7a98946d2365974752f424831068ab1528a2b540
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-15 21:21:08 +0200

    wip

--- src/Coal/Compiler/Pass/TypePhase/TypeInference.hs
+++ src/Coal/Compiler/Pass/TypePhase/TypeInference.hs
@@ -5,78 +5,38 @@
 
 module Coal.Compiler.Pass.TypePhase.TypeInference (passTypeInference) where
 
-import Coal.AST.Metadata (Metadata (..))
-import Coal.Common.Environment (Environment (..), mapEnvironment)
 import qualified Coal.Common.Environment as Environment
 import Coal.Compiler.Build (Build (..), buildNames)
-import Coal.Compiler.Build.Prep
-import Coal.Compiler.Builtin.Definitions (builtinFunctions)
-import Coal.Compiler.Journal (tellErrors)
+import Coal.Compiler.Build.Prep (replacePlaceholders)
 import Coal.Compiler.Pass (Pass (..))
 import Coal.Compiler.Stack
 import Coal.Compiler.State
 import Coal.Compiler.TypeInference (define, generateConstraints, generateKindConstraints, solveT)
-import Coal.Graphviz.Dot
-import Coal.Language (HasType (..), IndexedType, Kind, Trait (..), TypeIndex, indexed, instanceLabel, rowNormalize, typeOf)
+import Coal.Graphviz.Dot (generateDotSyntax)
+import Coal.Language (HasType (..), IndexedType, Kind, Trait (..), indexed, instanceLabel, rowNormalize, typeOf)
 import Coal.Language.Definition
-import Coal.Language.Module
-import Coal.Language.Module.Path
-import Coal.Language.Type (Type (..))
-import Coal.Language.Type.Kind.Indexed (ToKindIndexed (..))
-import Coal.TypeSystem.Constraint
+import Coal.Language.Module (Module (..))
+import Coal.Language.Module.Path (principalPath)
+import Coal.TypeSystem.Constraint (Constraint (Explicit))
 import Coal.TypeSystem.Constraint.Assumption (Assumption (..))
 import Coal.TypeSystem.Constraint.Generation.InferenceRule (InferenceRule (..))
 import Coal.TypeSystem.Kind.Constraint.Solver (solveKindConstraints)
 import Coal.TypeSystem.Kind.Substitution
-import Coal.TypeSystem.Kind.Unification
+import Coal.TypeSystem.Kind.Unification (KindUnifier (kindUnifierMonad))
 import Coal.TypeSystem.Substitution (apply, normalizeTypeIndexes)
-import Control.Monad.Except
+import Control.Monad.Except (MonadIO (..), forM, forM_)
 import Control.Monad.State (get, gets, modify, runState)
 import Data.Data (Data)
-import Data.List (nub)
-import qualified Data.Map as Map
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Data.Text.Lazy (toStrict)
-import Debug.Trace
-import Text.Pretty.Simple (pPrint, pShowNoColor)
-import TextShow (showt)
+import Text.Pretty.Simple (pShowNoColor)
 
 passTypeInference :: (MonadIO m, Monoid a, Data a, Eq a, Show a) => Pass a m (Module a Kind ()) (Module a Kind IndexedType)
-passTypeInference = Pass{runPass = pass}
+passTypeInference = Pass{runPass = passImpl}
 
-pass :: (MonadIO m, Monoid a, Data a, Eq a, Show a) => Module a Kind () -> CompilerT a m (Module a Kind IndexedType)
-pass m@(Module path _ _) = do
-  --  env <- buildEnv
-  --  setNamesC env
-  --  insertNamesC builtinFunctions
-
-  runTypeInference m
-
---  next <- runTypeInference m
---
---  names <- gets compilerNameStore
---  replacePlaceholders names
---
-----  assumptions <- lift $ gets (filter (not . isFoldAssumption) . nub . compilerAssumptions)
-----  forM_ assumptions $
-----    \Assumption{..} -> do
-----      tellErrors [NameNotInScope assumptionName (ErrorLocation (principalPath path) assumptionMetadata)]
-----  unless (null assumptions) $
-----    throwError NoSuchIdentifier
---
---  pure next
-
--- isFoldAssumption :: Assumption a t -> Bool
--- isFoldAssumption Assumption{..} = "!" `Text.isPrefixOf` assumptionName
-
--- indexTypes :: (Monad m, Traversable t) => t e -> CompilerT a m (t IndexedType)
--- indexTypes ds = undefined -- run (indexed ds) =<< gets compilerSupply
--- where
---  run s m = do
---    let (r, n) = runState s m
---    insertSupplyC n
---    pure r
+passImpl :: (MonadIO m, Monoid a, Data a, Eq a, Show a) => Module a Kind () -> CompilerT a m (Module a Kind IndexedType)
+passImpl = runTypeInference
 
 runTypeInference :: (MonadIO m, Monoid a, Data a, Eq a, Show a) => Module a Kind () -> CompilerT a m (Module a Kind IndexedType)
 runTypeInference m = do
@@ -93,7 +53,7 @@ runTypeInference m = do
 
   pure nm
 
-ti :: (MonadIO m, Data a, Monoid a, Show a, Eq a) => Module a Kind () -> CompilerT a m (Module a Kind IndexedType)
+ti :: (MonadIO m, Data a, Show a, Eq a) => Module a Kind () -> CompilerT a m (Module a Kind IndexedType)
 ti modul = do
   indexed <- inferKinds modul
   newModule <- inferTypes indexed
