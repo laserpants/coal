007f049ad23801853269e850fac71cdb474584b1
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-16 19:06:36 +0200

    wip

--- src/Coal/Compiler/Pass/PhasePreflight/DetectMisplacedImportStatements.hs
+++ src/Coal/Compiler/Pass/PhasePreflight/DetectMisplacedImportStatements.hs
@@ -2,6 +2,38 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 
+{- |
+Module: Coal.Compiler.Pass.PhasePreflight.DetectMisplacedImportStatements
+
+Detect @import@ statements that are not at the top of the module.
+
+This pass enforces the convention that all @import@ statements must appear at
+the beginning of a module, before any other definitions.
+
+For example, this is valid:
+
+@
+module MyModule
+
+import List(concat, head, tail)
+import String(is_empty)
+
+fun my_function() = ...
+@
+
+But this would be flagged as an error:
+
+@
+module MyModule
+
+fun my_function() = ...
+
+import List(concat, head, tail)  // Error: import after definition
+@
+
+The pass reports all misplaced @import@ statements, requiring them to be moved
+to the top of the module.
+-}
 module Coal.Compiler.Pass.PhasePreflight.DetectMisplacedImportStatements (
   passDetectMisplacedImportStatements,
 ) where
@@ -22,6 +54,12 @@ import Coal.Language.Module (Module (..))
 import Coal.Language.Module.Path (principalPath)
 import Control.Monad.Except (MonadError (throwError), MonadIO, forM_, unless)
 
+{- | Misplaced import detection pass.
+
+Validate that all @import@ statements appear at the top of the module before any
+other definitions. Report errors for any import statements found after
+non-import definitions, enforcing a consistent module structure.
+-}
 passDetectMisplacedImportStatements :: (MonadIO m) => Pass Metadata m [BuildEnvelope (Module Metadata () ())] [BuildEnvelope (Module Metadata () ())]
 passDetectMisplacedImportStatements = Pass{runPass = passImpl}
 
