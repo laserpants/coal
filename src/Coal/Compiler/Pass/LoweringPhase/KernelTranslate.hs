b533b98ed79440e9114dc9179aa55f5563d0a17c
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-17 13:59:00 +0200

    wip

--- src/Coal/Compiler/Pass/LoweringPhase/KernelTranslate.hs
+++ src/Coal/Compiler/Pass/LoweringPhase/KernelTranslate.hs
@@ -1,6 +1,8 @@
+-- +
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Coal.Compiler.Pass.LoweringPhase.KernelTranslate (passKernelTranslate) where
 
@@ -11,29 +13,35 @@ import Coal.Compiler.Build.Envelope (BuildEnvelope (..))
 import Coal.Compiler.Kernel.Environment (insertQualifiedNames, withModuleName)
 import Coal.Compiler.Kernel.Translate.Definition (translateDefinition)
 import Coal.Compiler.Pass (Pass (..), tickBar)
-import Coal.Compiler.Stack
+import Coal.Compiler.Stack (CompilerT, getCurrentBuildC, setCurrentPathC)
 import qualified Coal.Kernel.Language as Kernel
 import Coal.Language (IndexedType, Kind (..))
 import Coal.Language.Module (Module (Module))
 import Coal.Language.Module.Path (principalPath)
 import Control.Monad.IO.Class (MonadIO)
-import Extras (Name)
+import Extras (Name, concatForM)
 
 passKernelTranslate :: (MonadIO m) => Pass Metadata m (BuildEnvelope (Module Metadata Kind IndexedType)) (BuildEnvelope (Kernel.Module Kernel.Type Name (Kernel.Expr Kernel.Type)))
-passKernelTranslate = Pass{runPass = \p -> tickBar >> traverse pass p}
+passKernelTranslate = Pass{runPass = passImpl}
+
+passImpl :: (MonadIO m, Traversable t) => t (Module Metadata Kind IndexedType) -> CompilerT Metadata m (t (Kernel.Module Kernel.Type Name (Kernel.Expr Kernel.Type)))
+passImpl p = tickBar >> traverse pass p
 
 pass :: (MonadIO m) => Module Metadata Kind IndexedType -> CompilerT Metadata m (Kernel.Module Kernel.Type Name (Kernel.Expr Kernel.Type))
 pass =
   \case
     Module path _ defs -> do
       setCurrentPathC path
-      Build{..} <- getCurrentBuildC
+      Build{buildQualifiedNames} <- getCurrentBuildC
       insertQualifiedNames buildQualifiedNames $
-        withModuleName name $
-          Kernel.Module
-            name
-            (Environment.elems buildQualifiedNames)
-            . concat
-            <$> traverse translateDefinition defs
-     where
-      name = principalPath path
+        let
+          moduleName = principalPath path
+         in
+          withModuleName moduleName $ do
+            newObjects <- concatForM defs translateDefinition
+            return $
+              Kernel.Module
+                { moduleName = moduleName
+                , moduleImports = Environment.elems buildQualifiedNames
+                , moduleObjects = newObjects
+                }
