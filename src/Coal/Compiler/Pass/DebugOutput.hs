2aadb03664bcff0fdbfde8dc3d7dd149d915c82c
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-21 19:38:31 +0300

    wip

--- src/Coal/Compiler/Pass/DebugOutput.hs
+++ src/Coal/Compiler/Pass/DebugOutput.hs
@@ -11,6 +11,9 @@ module Coal.Compiler.Pass.DebugOutput where -- (generateDebugArtifacts) where
 import Coal.Compiler.Config (CompilerConfig (..))
 import Coal.Compiler.Pass (Pass (..))
 import Coal.Compiler.Stack
+import Coal.Compiler.State (compilerConfig)
+import Coal.Language
+import Coal.Language.Module.Path (Path (..))
 import Control.Monad (when)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.State (gets)
@@ -19,25 +22,27 @@ import qualified Data.Text as Text
 import Extras (forM_)
 import Prettyprinter (Pretty (..))
 
--- generateDebugArtifacts :: (MonadIO m, Pretty t, Show t) => Text -> Pass a m (Module a k t) (Module a k t)
--- generateDebugArtifacts ll = Pass{runPass = pass ll}
---
--- pass :: (MonadIO m, Pretty t, Show t) => Text -> Module a k t -> CompilerT a m (Module a k t)
--- pass label m = do
---  CompilerConfig{..} <- gets compilerConfig
---  when configGenerateDotFiles $
---    liftIO $
---      writeDotFiles label m
---  pure m
---
--- writeDotFiles :: (Pretty t, Show t) => Text -> Module a k t -> IO ()
--- writeDotFiles ns m@(Module (Path path) _ defs) = do
+generateDebugArtifacts :: (MonadIO m, Pretty t, Show t) => Text -> Pass a m (Module a k t) (Module a k t)
+generateDebugArtifacts ll = Pass{runPass = pass ll}
+
+pass :: (MonadIO m, Pretty t, Show t) => Text -> Module a k t -> CompilerT a m (Module a k t)
+pass label m = do
+  CompilerConfig{..} <- gets compilerConfig
+  when configGenerateDotFiles $
+    liftIO $
+      writeDotFiles label m
+  pure m
+
+writeDotFiles :: (Pretty t, Show t) => Text -> Module a k t -> IO ()
+writeDotFiles ns m@(Module (Path path) _ defs) = do
+  undefined
+
 --  writeDotFile prefix m
 --  forM_ defs $
 --    \case
 --      def@DFunction{} ->
 --        writeDotFile (prefixedName def) def
---      def@DConstant{} ->
+--      def@DLet{} ->
 --        writeDotFile (prefixedName def) def
 --      def@DFold{} ->
 --        writeDotFile (prefixedName def) def
