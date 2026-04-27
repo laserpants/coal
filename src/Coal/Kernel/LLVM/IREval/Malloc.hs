ebe350624b61f2445c469f9065ab45f28225f4a6
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-04-27 18:15:41 +0300

    wip

--- src/Coal/Kernel/LLVM/IREval/Malloc.hs
+++ src/Coal/Kernel/LLVM/IREval/Malloc.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Coal.Kernel.LLVM.IREval.Malloc (irMalloc) where
+module Coal.Kernel.LLVM.IREval.Malloc (irMalloc, irMallocN) where
 
 import Coal.Kernel.LLVM.IREval.Comment (irCommentBlock)
 import Coal.Kernel.LLVM.IRInstruction (IRInstr)
@@ -13,6 +13,14 @@ irMalloc :: IRType -> IRInstr IRValue
 irMalloc t = do
   irCommentBlock "gc_malloc" $ do
     r1 <- gepsize t (I32 1)
+    r2 <- ptrtoint r1 i64
+    r3 <- callg i8Ptr "gc_malloc" [r2]
+    bitcast r3 (ptr t)
+
+irMallocN :: IRType -> IRValue -> IRInstr IRValue
+irMallocN t n = do
+  irCommentBlock "gc_malloc" $ do
+    r1 <- gepsize t n
     r2 <- ptrtoint r1 i64
     r3 <- callg i8Ptr "gc_malloc" [r2]
     bitcast r3 (ptr t)
