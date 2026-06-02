3c2bb0144cff93cc6e8462e528c36d8e84360c4f
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-06-02 19:33:24 +0300

    wip

--- test/E2E/Spec.hs
+++ test/E2E/Spec.hs
@@ -1987,6 +1987,12 @@ e2eSpec = do
       "test/Coal/examples/380"
       ["Main.coal"]
 
+  describe "381" $ do
+    expectOutput
+      "8"
+      "test/Coal/examples/381"
+      ["Main.coal"]
+
 expectOutput :: String -> String -> [FilePath] -> Spec
 expectOutput expt srcPath files =
   it ("\"" <> expt <> "\"") $ do
