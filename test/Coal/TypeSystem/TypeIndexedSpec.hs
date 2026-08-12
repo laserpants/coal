47de862fe14ad98cb979ce8e37387e5fe73a87ef
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2026-08-13 00:19:28 +0200

    Remove unused import

--- test/Coal/TypeSystem/TypeIndexedSpec.hs
+++ test/Coal/TypeSystem/TypeIndexedSpec.hs
@@ -3,7 +3,6 @@
 
 module Coal.TypeSystem.TypeIndexedSpec (typeIndexedSpec, typeIndexedTrickySpec) where
 
-import Coal.Common.Label (Label (..))
 import Coal.Language
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NonEmpty
