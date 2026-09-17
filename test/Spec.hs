6d086985ef7711d446b8e9c8a1b479de4669585e
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2026-09-17 20:34:37 +0200

    Improve error message when conflicting version requirements

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,7 @@ import Coal.Kernel.Spec (kernelSpec)
 import Coal.Language.TypeSpec (typeApplicationSpec, typeArgsSpec)
 import Coal.TypeSystemSpec (typeSystemSpec)
 import E2E.Spec (e2eSpec)
+import Package.ConstraintValidationSpec (constraintValidationSpec)
 import Package.VersionSpec (versionSpec)
 import Test.Hspec (SpecWith, describe, hspec)
 
@@ -27,6 +28,7 @@ main =
     describe "CLI tests" $ do
       addCmdSpec
       buildSpec
+      constraintValidationSpec
       versionSpec
 
     describe "Kernel tests" kernelSpec
